209cf88fe7e9d04ab3e41c203095ff559cfc74c5
Ekaterina1994 <katishteno@mail.ru>
2023-08-27 23:28:37 +0300

    refactor: add drawio.bkp to gitignore

---
+++ mw-webapp/src/model/firebaseCollection/DayReport.ts
@@ -0,0 +1,63 @@
+/**
+ * Day's report
+ */
+export class DayReport {
+
+  /**
+   * Day report's UUID
+   */
+  public uuid: string;
+
+  /**
+   * Report's date
+   */
+  public date: string;
+
+  /**
+   * Array of @JobDone.uuid
+   */
+  public jobDone: string[];
+
+  /**
+   * Array of @PlanForNextPeriod.uuid
+   */
+  public planForNextPeriod: string[];
+
+  /**
+   * Array of @ProblemsForCurrent.uuid
+   */
+  public problemsForCurrentPeriod: string[];
+
+  /**
+   * Anything that student wants to say about work
+   */
+  public studentComment: string[];
+
+  /**
+   * New knowledge that the user has received
+   */
+  public learnedForToday: string[];
+
+  /**
+   * Array of @MentorComment.uuid
+   */
+  public mentorComment: string[];
+
+  /**
+   * Return true if day is off and false if it is work day
+   */
+  public isDayOff: boolean;
+
+  constructor(dayReportData: DayReport) {
+    this.uuid = dayReportData.uuid;
+    this.date = dayReportData.date;
+    this.jobDone = dayReportData.jobDone;
+    this.planForNextPeriod = dayReportData.planForNextPeriod;
+    this.problemsForCurrentPeriod = dayReportData.problemsForCurrentPeriod;
+    this.studentComment = dayReportData.studentComment;
+    this.learnedForToday = dayReportData.learnedForToday;
+    this.mentorComment = dayReportData.mentorComment;
+    this.isDayOff = dayReportData.isDayOff;
+  }
+
+}
