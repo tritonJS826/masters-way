375335d08cd04ecc8d70ebfa066e03253c1538b6
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-09-04 08:31:47 +0500

    refactor: create getEnvs object

--- mw-webapp/vite.config.ts
+++ mw-webapp/vite.config.ts
@@ -6,16 +6,28 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => {
   const env = loadEnv("", process.cwd(), "");
 
+  const getEnvs =
+    Object.entries(env).filter(([key]) => key.includes("FIREBASE")).reduce(
+      (prev, [key, val]) => {
+        return {
+          ...prev,
+          [`import.meta.env.${key}`]: JSON.stringify(val),
+        };
+      },
+      {},
+    );
+
   return {
     build: {outDir: "build"},
     plugins: [react(), eslint(), viteTsconfigPaths()],
-    define: {
-      "import.meta.env.AUTH_FIREBASE_API_KEY": JSON.stringify(env.AUTH_FIREBASE_API_KEY),
-      "import.meta.env.AUTH_FIREBASE_AUTH_DOMAIN": JSON.stringify(env.AUTH_FIREBASE_AUTH_DOMAIN),
-      "import.meta.env.AUTH_FIREBASE_PROJECT_ID": JSON.stringify(env.AUTH_FIREBASE_PROJECT_ID),
-      "import.meta.env.AUTH_FIREBASE_STORAGE_BUCKET": JSON.stringify(env.AUTH_FIREBASE_STORAGE_BUCKET),
-      "import.meta.env.AUTH_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(env.AUTH_FIREBASE_MESSAGING_SENDER_ID),
-      "import.meta.env.AUTH_FIREBASE_APP_ID": JSON.stringify(env.AUTH_FIREBASE_APP_ID),
-    },
+    define: getEnvs,
+    // define: {
+    //   "import.meta.env.AUTH_FIREBASE_API_KEY": JSON.stringify(env.AUTH_FIREBASE_API_KEY),
+    //   "import.meta.env.AUTH_FIREBASE_AUTH_DOMAIN": JSON.stringify(env.AUTH_FIREBASE_AUTH_DOMAIN),
+    //   "import.meta.env.AUTH_FIREBASE_PROJECT_ID": JSON.stringify(env.AUTH_FIREBASE_PROJECT_ID),
+    //   "import.meta.env.AUTH_FIREBASE_STORAGE_BUCKET": JSON.stringify(env.AUTH_FIREBASE_STORAGE_BUCKET),
+    //   "import.meta.env.AUTH_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(env.AUTH_FIREBASE_MESSAGING_SENDER_ID),
+    //   "import.meta.env.AUTH_FIREBASE_APP_ID": JSON.stringify(env.AUTH_FIREBASE_APP_ID),
+    // },
   };
 });
