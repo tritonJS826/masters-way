ae21814540cd6974e94615e7e60a9b9c8a338477
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-09-11 21:18:08 +0500

    refactor: parameter name

--- mw-webapp/src/utils/auth/handleUserAuthState.ts
+++ mw-webapp/src/utils/auth/handleUserAuthState.ts
@@ -1,8 +1,8 @@
 import {User, onAuthStateChanged} from "firebase/auth";
 import {auth} from "src/firebase";
 
-export const handleUserAuthState = (setUserFunction: React.Dispatch<React.SetStateAction<User | null>>) => {
+export const handleUserAuthState = (setUser: React.Dispatch<React.SetStateAction<User | null>>) => {
   onAuthStateChanged(auth, (currentUser) => {
-    setUserFunction(currentUser ?? null);
+    setUser(currentUser ?? null);
   });
 };
