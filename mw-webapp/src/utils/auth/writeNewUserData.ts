5ee0680915ac651b6f44b1d746f722789e4ee6cf
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-09-11 21:16:51 +0500

    refactor: interface syntax

--- mw-webapp/src/utils/auth/writeNewUserData.ts
+++ mw-webapp/src/utils/auth/writeNewUserData.ts
@@ -2,9 +2,9 @@ import {ref, set} from "firebase/database";
 import {db} from "src/firebase";
 
 interface UserData {
-  userId: string
-  userEmail: string | null
-  userName: string | null
+  userId: string;
+  userEmail: string | null;
+  userName: string | null;
 }
 
 export const writeNewUserData = ({userId, userEmail, userName}: UserData) => {
