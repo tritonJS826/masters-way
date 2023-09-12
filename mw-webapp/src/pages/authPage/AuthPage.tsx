a9b69e8cf10c5bcedd946d7f8a3fed9a2d43cbbd
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-09-12 07:41:32 +0500

    refactor: use Navigate element instead of useNavigate

--- mw-webapp/src/pages/authPage/AuthPage.tsx
+++ mw-webapp/src/pages/authPage/AuthPage.tsx
@@ -1,6 +1,6 @@
 import {User} from "firebase/auth";
 import {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
 import {Button} from "src/component/button/Button";
 import {handleUserCredentials} from "src/utils/auth/handleUserCredentials";
 import {handleUserAuthState} from "src/utils/auth/handleUserAuthState";
@@ -14,7 +14,6 @@ const MAIN_PATH = "/main";
 
 export const AuthPage = () => {
   const [user, setUser] = useState<User | null>(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
     handleUserAuthState(setUser);
@@ -23,18 +22,17 @@ export const AuthPage = () => {
 
   return (
     <>
-      {user ?
-        navigate(MAIN_PATH) : (
-          <>
-            <Button
-              value={user ? BUTTON_LOG_OUT_VALUE : BUTTON_LOG_IN_VALUE}
-              onClick={user ? handleLogOut : handleLogIn}
-            />
-            <Link to={MAIN_PATH}>
-              {LINK_TEXT}
-            </Link>
-          </>
-        )}
+      {user && <Navigate to={MAIN_PATH} /> }
+      <>
+        <Button
+          value={user ? BUTTON_LOG_OUT_VALUE : BUTTON_LOG_IN_VALUE}
+          onClick={user ? handleLogOut : handleLogIn}
+        />
+        <Link to={MAIN_PATH}>
+          {LINK_TEXT}
+        </Link>
+      </>
+
     </>
   );
 };
