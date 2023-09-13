124c0d573c93b8fb64f8f7e89277f586d57af4f1
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-09-13 19:28:49 +0500

    refactor: add method to UserService that writes new user instead of separate function

--- mw-webapp/src/utils/auth/handleUserCredentials.ts
+++ mw-webapp/src/utils/auth/handleUserCredentials.ts
@@ -1,16 +1,29 @@
 import {getRedirectResult} from "firebase/auth";
-import {writeNewUserData} from "src/utils/auth/writeNewUserData";
 import {auth} from "src/firebase";
+import {UserService} from "src/service/UserService";
+
+const newUserBlankWays = {
+  ownWays: [""],
+  favoriteWays: [""],
+  mentoringWays: [""],
+};
 
 export const handleUserCredentials = async () => {
   try {
     const userCredentials = await getRedirectResult(auth);
-    if (userCredentials) {
-      writeNewUserData(
-        {userId: userCredentials.user.uid, userEmail: userCredentials.user.email, userName: userCredentials.user.displayName},
-      );
-    } else {
-      return;
+    if(userCredentials) {
+      if (userCredentials.user.email && userCredentials.user.displayName) {
+        UserService.writeNewUserData(
+          {
+            uuid: userCredentials.user.uid,
+            email: userCredentials.user.email,
+            name: userCredentials.user.displayName,
+            ...newUserBlankWays,
+          },
+        );
+      } else {
+        return;
+      }
     }
   } catch (error) {
     alert(error ?? null);
