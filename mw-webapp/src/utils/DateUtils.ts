3330f219cf3c46deb28b0f328b2ea274677c063a
Ekaterina1994 <katishteno@mail.ru>
2023-09-19 08:39:59 +0300

    fix: #37 rename consts

--- mw-webapp/src/utils/DateUtils.ts
+++ mw-webapp/src/utils/DateUtils.ts
@@ -1,8 +1,6 @@
-const START_DATE_RANGE_SYMBOL = 0;
-const END_DATE_RANGE_SYMBOL = 10;
-const START_OF_DATE_SYMBOL_RANGE = 0;
-const END_OF_DATE_SYMBOL_RANGE = 1;
-const DATE_SYMBOL_RANGE: [number, number] = [START_DATE_RANGE_SYMBOL, END_DATE_RANGE_SYMBOL];
+const START_OF_ISO_SUBSTRING_RANGE = 0;
+const END_OF_ISO_SUBSTRING_RANGE = 10;
+const DATE_FROM_ISO_SUBSTRING_RANGE: [number, number] = [START_OF_ISO_SUBSTRING_RANGE, END_OF_ISO_SUBSTRING_RANGE];
 
 /**
  * Formatted date
@@ -13,7 +11,9 @@ export class DateUtils {
    * Format date to string yyyy-mm-dd
    */
   public static getShortISODateValue(date: Date): string {
-    return date.toISOString().slice(DATE_SYMBOL_RANGE[START_OF_DATE_SYMBOL_RANGE], DATE_SYMBOL_RANGE[END_OF_DATE_SYMBOL_RANGE]);
+    return date
+      .toISOString()
+      .substring(...DATE_FROM_ISO_SUBSTRING_RANGE);
   }
 
 }
