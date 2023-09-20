31943e2774fc40bdee3b7aee8b92fb10d0002be9
Lelik Jan <21alex7777@gmail.com>
2023-09-20 17:52:30 +0300

    feat: add component Select.tsx Select.module.scss

---
+++ mw-webapp/src/component/select/Select.tsx
@@ -0,0 +1,50 @@
+import styles from "src/component/select/Select.module.scss";
+import {useState} from "react";
+
+interface SelectProps {
+  /**
+   * Label's text;
+   */
+  label: string;
+  /**
+   * Default select value (string)
+   */
+  value: string;
+  /**
+   * Select's name (string)
+   */
+  name: string;
+  /**
+   * Options list (Array<OptionType>)
+   */
+  options: OptionType[];
+}
+interface OptionType {
+  id: string;
+  value: string;
+  text: string;
+}
+
+export const Select: React.FC<SelectProps> = (props: SelectProps) => {
+  const [selected, setSelected] = useState(props.value);
+  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => setSelected(e.target.value);
+  return (
+    <label>
+      {props.label}
+      <select name={props.name}
+        defaultValue={selected}
+        onChange={handleOnChange}
+        className={styles.select}
+      >
+        {props.value}
+        {props.options.map((option) => (
+          <option key={option.id}
+            value={option.value}
+          >
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
