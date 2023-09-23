570eefede2ea004f26de839c518b019dcf145d3a
chelovek-zeml <stranger37561@gmail.com>
2023-09-23 13:53:59 +0300

    fix: #56 changed component in the Page404

--- mw-webapp/src/pages/Page404/Page404.tsx
+++ mw-webapp/src/pages/Page404/Page404.tsx
@@ -1,16 +1,14 @@
-import {Heading} from "@radix-ui/themes";
-import {HeadingLevel} from "src/component/title/Title";
+import {HeadingLevel, Title} from "src/component/title/Title";
 import styles from "src/pages/page404/Page404.module.scss";
 
 const ERROR_404 = "404 NOT FOUND";
 
 export const Page404 = () => {
   return (
-    <Heading
-      as={HeadingLevel.h2}
+    <Title
+      level={HeadingLevel.h2}
       className={styles.errorPageContainer}
-    >
-      {ERROR_404}
-    </Heading>
+      text={ERROR_404}
+    />
   );
 };
