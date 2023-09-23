e5013641ad62786966cf21d855cba4855913196e
Aleksei Erchenko <proger@a1exxuss.ru>
2023-09-23 21:14:07 +0300

    refactor: refactor input component

--- mw-webapp/src/component/input/Input.tsx
+++ mw-webapp/src/component/input/Input.tsx
@@ -1,39 +1,53 @@
 import {FC, HTMLInputTypeAttribute} from "react";
-import cn from "classnames";
-import styles from "./Input.module.scss";
+import styles from "src/component/input/Input.module.scss";
 
-
+/**
+ * The `InputProps` interface represents a set of properties that can be passed to the `Input` component.
+ */
 interface InputProps {
+  /**
+   * The input type of the input field. It can be one of the following values: "text", "tel", "url", "email", "numeric",
+   * "decimal", "search", or undefined.
+   */
   type?: HTMLInputTypeAttribute;
+
+  /**
+   * The placeholder text for the input field.
+   */
   placeholder?: string;
-  className?: string | Array<string>;
+
+  /**
+   * The input mode of the field. It can be one of the following values: "none", "text", "tel", "url", "email", "numeric",
+   * "decimal", "search", or undefined.
+   */
   inputMode?: "none" | "text" | "tel" | "url" | "email" | "numeric" | "decimal" | "search" | undefined;
+
+  /**
+   * A flag indicating whether the input field should be disabled (unavailable for input).
+   */
   disabled?: boolean;
+
+  /**
+   * A flag indicating whether the input field is required.
+   */
   required?: boolean;
+
+  /**
+   * The event handler for the input field's change event.
+   */
+  onChange: () => void;
 }
 
-export const Input: FC<InputProps> = ({
-  type = "text",
-  placeholder = "",
-  className = "",
-  disabled = false,
-  required = false,
-  inputMode,
-}) => {
+export const Input: FC<InputProps> = (props) => {
   return (
-    <div className={styles.input__wrapper}>
-      <label
-        className={styles["visually-hidden"]}
-      >
-        {placeholder}
-      </label>
-      <input type={type}
-        placeholder={placeholder}
-        className={cn(styles.input, [className])}
-        inputMode={inputMode}
-        disabled={disabled}
-        required={required}
-      />
-    </div>
+    <input
+      type={props.type || "text"}
+      placeholder={props.placeholder}
+      className={styles.input}
+      inputMode={props.inputMode}
+      disabled={props.disabled || false}
+      required={props.required || false}
+      onChange={props.onChange}
+    />
   );
 };
