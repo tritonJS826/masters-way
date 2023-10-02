ff908cf88cb25aba04ac2e946131d3e44c477744
Shekerbekov Islam <113792107+Pivodur@users.noreply.github.com>
2023-10-02 23:27:12 +0600

    feat: #82 add image component (#111)

---
+++ mw-webapp/src/component/image/Image.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "src/component/image/Image.module.scss";
+
+/**
+ * Props for the Image component
+ */
+interface ImageProps {
+  /**
+   * Image source
+   */
+  src: string;
+  /**
+   * Image alt text
+   */
+  alt: string;
+  /**
+   * Additional custom class name for the component
+   */
+  className?: string;
+}
+
+/**
+ * Сomponent for displaying images
+ */
+export const Image: React.FC<ImageProps> = (props: ImageProps) => {
+  return (
+    <div className={props.className}>
+      <img
+        src={props.src}
+        alt={props.alt}
+        className={styles.image}
+      />
+    </div>);
+};
+
+
