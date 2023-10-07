b724993e30cedb19a5b7476925d8ca18fc7b2c46
Ekaterina1994 <katishteno@mail.ru>
2023-10-07 16:44:38 +0300

    feat: #85 Create input component

---
+++ mw-webapp/src/component/input/InputMode.ts
@@ -0,0 +1,46 @@
+/**
+ * Input's modes for virtual keyboard
+ * @public
+ */
+export enum InputMode {
+
+  /**
+   * Without virtual keyboard
+   */
+  none = "none",
+
+  /**
+   * Text keyboard
+   */
+  text = "text",
+
+  /**
+   * A telephone keyboard, include the digits 0–9, the asterisk (*), and the pound (#) key
+   */
+  tel = "tel",
+
+  /**
+   * A keyboard for URLs, may have the "/" key
+   */
+  url = "url",
+
+  /**
+   * A keyboard for emails. Includes the "@"
+   */
+  email = "email",
+
+  /**
+   * Numeric keyboard requires the digits 0–9
+   */
+  numeric = "numeric",
+
+  /**
+   * Numeric keyboard containing the digits and decimal separator (".", ",")
+   */
+  decimal = "decimal",
+
+  /**
+   * A virtual keyboard optimized for search input
+   */
+  search = "search",
+}
