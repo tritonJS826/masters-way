65ee4d0d761f1671b768bac87a0e16f9ddc478b2
Ekaterina1994 <katishteno@mail.ru>
2023-10-07 14:19:35 +0300

    fix: #117 Add storybook-static to gitinbore, rewrite comments

--- mw-webapp/src/service/auth/logIn.ts
+++ mw-webapp/src/service/auth/logIn.ts
@@ -2,7 +2,7 @@ import {signInWithRedirect} from "firebase/auth";
 import {auth, provider} from "src/firebase";
 
 /**
- * Call firebase's method for log in
+ * Call method for log in
  */
 export const logIn = async () => {
   await signInWithRedirect(auth, provider);
--- mw-webapp/src/service/auth/logOut.ts
+++ mw-webapp/src/service/auth/logOut.ts
@@ -2,7 +2,7 @@ import {signOut} from "firebase/auth";
 import {auth} from "src/firebase";
 
 /**
- * Call firebase's method for logout
+ * Call method for logout
  */
 export const logOut = async () => {
   await signOut(auth);
