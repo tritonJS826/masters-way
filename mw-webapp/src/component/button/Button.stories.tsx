72c1613613ee8d54cd69cec3c96673811d9a3abf
Ekaterina1994 <katishteno@mail.ru>
2023-10-07 14:30:31 +0300

    fix: #117 Add storybook-static to gitinbore, rewrite comments

--- mw-webapp/src/component/button/Button.stories.tsx
+++ mw-webapp/src/component/button/Button.stories.tsx
@@ -12,7 +12,19 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {args: {value: "Button", onClick: () => {}}};
+export const Default: Story = {
+  args: {
+    value: "Button",
+    onClick: () => {},
+  },
+};
 
-export const Submit: Story = {args: {value: "Submit", onClick: () => {}}};
+export const Submit: Story = {
+  args: {
+    value: "Submit", /**
+Eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee *
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee */
+    onClick: () => {},
+  },
+};
 
