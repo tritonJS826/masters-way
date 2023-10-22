02c4a2cb243eeb370d1a7fa7e5a84686b2dd7be6
Assimbayev Marat <chesterfild-9@mail.ru>
2023-10-22 21:24:40 +0600

    refactor: #150 rename  accordion items

--- mw-webapp/src/logic/aboutProjectPage/AboutProjectPage.tsx
+++ mw-webapp/src/logic/aboutProjectPage/AboutProjectPage.tsx
@@ -1,4 +1,3 @@
-
 import {Accordion, accordionTypes} from "src/component/accordion/Accordion";
 import {AccordionContent} from "src/component/accordion/AccordionContent/AccordionContent";
 import {AccordionTrigger} from "src/component/accordion/AccordionTrigger/AccordionTrigger";
@@ -14,8 +13,28 @@ export const AboutProjectPage = () => {
    * Accordion props
    */
   const accordionItems = [
-    {trigger: <AccordionTrigger text="What is your name" />, content: <AccordionContent text="Master" />},
-    {trigger: <AccordionTrigger text="What is your last name" />, content: <AccordionContent text="Way" />},
+    {
+      trigger: <AccordionTrigger text="What is the Master's Way?" />,
+      content:
+  <AccordionContent text="Master's Way is an app to track your learning.
+	 You can record your progress, set goals, and share data with
+	 a mentor or other users."
+  />,
+    },
+    {
+      trigger: <AccordionTrigger text="How can I start using the application?" />,
+      content:
+  <AccordionContent text="First, you need to register and create an account.
+	 After this you can start recording your Way"
+  />,
+    },
+    {
+      trigger: <AccordionTrigger text="How can I share data with a mentor?" />,
+      content:
+  <AccordionContent text="In your profile settings, you can add a
+  mentor contact and give him access to your data. He will be able to see your progress and help you."
+  />,
+    },
   ];
 
   return (
@@ -24,19 +43,17 @@ export const AboutProjectPage = () => {
         level={HeadingLevel.h2}
         text="About project page"
       />
-      <>
-        <Title
-          level={HeadingLevel.h3}
-          text="FAQ accordion"
-          className={styles.titleAccordion}
-        />
-        <Accordion
-          items={accordionItems}
-          type={accordionTypes.multiple}
-          className={styles.accordion}
-        />
-      </>
 
+      <Title
+        level={HeadingLevel.h3}
+        text="FAQ"
+        className={styles.titleAccordion}
+      />
+      <Accordion
+        items={accordionItems}
+        type={accordionTypes.multiple}
+        className={styles.accordion}
+      />
     </>
   );
 };
