3d9407c60e46054da64c50cd8da02adfa7392dfb
tritonJS826 <triton.68@mail.ru>
2023-11-04 21:16:32 +0400

    feat: #157 add mechanism to check env variables

--- mw-webapp/src/firebase.ts
+++ mw-webapp/src/firebase.ts
@@ -1,15 +1,16 @@
 import {initializeApp} from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
+import {env} from "src/utils/env/env";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.FIREBASE_DATABASE_URL,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
