4f1826376ef836a18ff84d4be96a62027753a04d
Viktar Veratsennikau <56690686+tritonJS826@users.noreply.github.com>
2023-11-04 23:18:04 +0400

    feat: #157 add mechanism to check env variables (#181)

--- mw-webapp/vite.config.ts
+++ mw-webapp/vite.config.ts
@@ -2,28 +2,12 @@ import react from "@vitejs/plugin-react";
 import {defineConfig, loadEnv} from "vite";
 import eslint from "vite-plugin-eslint";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { cleanEnv } from 'envalid'
+import { envSchema } from "./src/utils/env/envSchema";
 
 export default defineConfig(() => {
   const env = loadEnv("", process.cwd(), "");
-  const envNames = [
-    "FIREBASE_API_KEY",
-    "FIREBASE_AUTH_DOMAIN",
-    "FIREBASE_DATABASE_URL",
-    "FIREBASE_PROJECT_ID",
-    "FIREBASE_STORAGE_BUCKET",
-    "FIREBASE_MESSAGING_SENDER_ID",
-    "FIREBASE_APP_ID",
-  ];
-
-  /**
-   * Return envs
-   */
-  const getEnvs = (envs: string[]) => envs.reduce((prev, envName) => {
-    return {
-      ...prev,
-      [`import.meta.env.${envName}`]: JSON.stringify(env[envName]),
-    };
-  }, {});
+  const validatedEnvs = cleanEnv(env, envSchema);
 
   return {
     build: {target: "esnext", outDir: "build"},
@@ -32,10 +16,14 @@ export default defineConfig(() => {
       eslint(
         // Exclude "virtual" to fix bug with vite-plugin-eslint and Storybook
         // https://github.com/storybookjs/builder-vite/issues/535#issuecomment-1507352550
-        {exclude: ["/virtual:/**"], failOnError: false},
+        {
+          exclude: ["/virtual:/**"], 
+          failOnError: false},
       ),
       viteTsconfigPaths(),
     ],
-    define: getEnvs(envNames),
+    define: {
+      'process.env': validatedEnvs,
+    },
   };
 });
