43137aea86ee1cce53a4f28dcced79374cfb5a28
Alexander Shustovsky <99250085+Shustovsky@users.noreply.github.com>
2023-11-09 10:47:31 +0400

    feat: #127 Add Sidebar on all pages (#186)

--- mw-webapp/src/component/header/Header.tsx
+++ mw-webapp/src/component/header/Header.tsx
@@ -1,7 +1,10 @@
 import {useEffect, useState} from "react";
 import {User} from "firebase/auth";
 import {Button} from "src/component/button/Button";
+import {Link} from "src/component/link/Link";
+import {Sidebar} from "src/component/sidebar/Sidebar";
 import {HeadingLevel, Title} from "src/component/title/Title";
+import {pages} from "src/router/pages";
 import {handleUserAuthState} from "src/service/auth/handleUserAuthState";
 import {logIn} from "src/service/auth/logIn";
 import {logOut} from "src/service/auth/logOut";
@@ -12,10 +15,68 @@ const BUTTON_LOG_OUT_VALUE = "Logout";
 const LOGO_TEXT = "master's way";
 
 /**
+ * Navigation link props.
+ */
+interface navigationLink {
+
+  /**
+   * Navigation link path.
+   */
+  path: string;
+
+  /**
+   * Navigation link value.
+   */
+  value: string;
+}
+
+/**
+ * Renders navigation links based on the provided navigationLinks array.
+ */
+const renderNavigationLinks = (navigationLinks: (navigationLink | null)[]) => {
+  return navigationLinks.map((item) => (
+    item && (
+      <Link
+        key={item.value}
+        path={item.path}
+        value={item.value}
+      />
+    )
+  ));
+};
+
+/**
  * Header component
  */
 export const Header = () => {
   const [user, setUser] = useState<User | null>(null);
+
+  const navigationLinks: (navigationLink | null)[] = [
+    {
+      path: pages.allWays.path,
+      value: "All ways",
+    },
+    {
+      path: pages.allUsers.path,
+      value: "All users",
+    },
+    user && {
+      path: pages.way.path(user.uid),
+      value: "Workflow",
+    },
+    user && {
+      path: pages.user.path(user.uid),
+      value: "My ways",
+    },
+    user && {
+      path: pages.userProfile.path(user.uid),
+      value: "Profile",
+    },
+    {
+      path: pages.aboutProject.path,
+      value: "About",
+    },
+  ];
 
   useEffect(() => {
     handleUserAuthState(setUser);
@@ -33,10 +94,26 @@ export const Header = () => {
           text={`Hello, ${user.displayName}!`}
         />
       }
-      <Button
-        onClick={user ? logOut : logIn}
-        value={user ? BUTTON_LOG_OUT_VALUE : BUTTON_LOG_IN_VALUE}
-      />
+      <div>
+        <Button
+          onClick={user ? logOut : logIn}
+          value={user ? BUTTON_LOG_OUT_VALUE : BUTTON_LOG_IN_VALUE}
+        />
+        <Sidebar
+          trigger={
+            <Button
+              value={"Navigation"}
+              onClick={() => {
+              }}
+            />
+          }
+          content={
+            <div className={styles.navSidebarContent}>
+              {renderNavigationLinks(navigationLinks)}
+            </div>
+          }
+        />
+      </div>
     </div>
   );
 };
