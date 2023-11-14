0d1e3358dda9fd3eb664d6beeebdb0511dd4c8de
Alexander Shustovsky <99250085+Shustovsky@users.noreply.github.com>
2023-11-14 13:42:42 +0400

    fix: #206 Fix sidebar component (#210)
    * fix: #206 fix sidebar
    
    * fix: #206 change z-index
    
    * fix: #206 fix classname
    
    * fix: #206 add close sidebar when click to link

--- mw-webapp/src/component/sidebar/SidebarContent/SidebarContent.tsx
+++ mw-webapp/src/component/sidebar/SidebarContent/SidebarContent.tsx
@@ -9,13 +9,37 @@ import {Cross2Icon} from "@radix-ui/react-icons";
 import styles from "src/component/sidebar/SidebarContent/SidebarContent.module.scss";
 
 /**
+ * SidebarContent props
+ */
+interface SidebarContentProps extends PropsWithChildren {
+
+  /**
+   * Callback triggered on SidebarContent click
+   */
+  onClick: () => void;
+}
+
+/**
  * A container for the content to be displayed within a Sidebar.
  */
-export const SidebarContent = (props: PropsWithChildren) => {
+export const SidebarContent = (props: SidebarContentProps) => {
+
+  /**
+   * Handle onClick event
+   */
+  const onClickHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target instanceof HTMLAnchorElement) {
+      props.onClick();
+    }
+  };
+
   return (
     <DialogPortal>
       <DialogOverlay className={styles.dialogOverlay} />
-      <DialogContent className={styles.dialogContent}>
+      <DialogContent
+        className={styles.dialogContent}
+        onClick={onClickHandler}
+      >
         {props.children}
         <DialogClose asChild>
           <button className={styles.closeButton}>
