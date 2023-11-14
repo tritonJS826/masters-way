8ee8858aa32ee01aaa6cefbadc58d9c62e7831e4
Ekaterina1994 <katishteno@mail.ru>
2023-11-14 13:29:30 +0300

    feat: #191 Add user's name for page

--- mw-webapp/src/logic/userPage/UserPage.tsx
+++ mw-webapp/src/logic/userPage/UserPage.tsx
@@ -1,5 +1,7 @@
+import {useEffect, useState} from "react";
 import {Navigate, useParams} from "react-router-dom";
 import {HeadingLevel, Title} from "src/component/title/Title";
+import {UserPreviewDAL} from "src/dataAccessLogic/UserPreviewDAL";
 import {QueryParamTypes} from "src/logic/QueryParamTypes";
 import {FavoriteWaysTable} from "src/logic/waysTable/FavoriteWaysTable";
 import {MentoringWaysTable} from "src/logic/waysTable/MentoringWaysTable";
@@ -12,12 +14,29 @@ import styles from "src/logic/userPage/UserPage.module.scss";
  */
 export const UserPage = () => {
   const {uuid} = useParams<QueryParamTypes>();
+  const [userName, setUserName] = useState<string>("");
+
+  if (!uuid) {
+    throw new Error("User is not exist");
+  }
+
+  /**
+   * Set user name
+   */
+  const getUser = async () => {
+    const user = await UserPreviewDAL.getUserPreview(uuid);
+    setUserName(user.name);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <div className={styles.container}>
       <Title
         level={HeadingLevel.h2}
-        text="User page"
+        text={`Page of ${userName}`}
       />
       {uuid
         ? (
@@ -27,13 +46,11 @@ export const UserPage = () => {
               level={HeadingLevel.h3}
             />
             <OwnWaysTable uuid={uuid} />
-
             <Title
               text="Mentoring ways"
               level={HeadingLevel.h3}
             />
             <MentoringWaysTable uuid={uuid} />
-
             <Title
               text="Favorite ways"
               level={HeadingLevel.h3}
