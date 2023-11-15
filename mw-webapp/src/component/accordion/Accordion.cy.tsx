47f00c48624a47d864139542b01e5eb2ff09c86a
dayanych <dayana.shakenova20@gmail.com>
2023-11-15 09:21:06 +0600

    feat: #188 add constants instead of strings

--- mw-webapp/src/component/accordion/Accordion.cy.tsx
+++ mw-webapp/src/component/accordion/Accordion.cy.tsx
@@ -4,29 +4,34 @@ import {AccordionTrigger} from "src/component/accordion/AccordionTrigger/Accordi
 import {getDataCy} from "src/helpers/getDataCy";
 
 describe("Accordion component", () => {
+  const FIRST_TRIGGER = "trigger1";
+  const SECOND_TRIGGER = "trigger2";
+  const FIRST_CONTENT = "content1";
+  const SECOND_CONTENT = "content2";
+
   const ACCORDION_ITEMS_EXAMPLE = [
     {
       trigger: <AccordionTrigger
-        text="Trigger1"
-        dataCy="trigger1"
+        text={FIRST_TRIGGER}
+        dataCy={FIRST_TRIGGER}
       />,
       content: <AccordionContent
         content={<div>
-          Content1
+          {FIRST_CONTENT}
         </div>}
-        dataCy="content1"
+        dataCy={FIRST_CONTENT}
       />,
     },
     {
       trigger: <AccordionTrigger
-        text="Trigger2"
-        dataCy="trigger2"
+        text={SECOND_TRIGGER}
+        dataCy={SECOND_TRIGGER}
       />,
       content: <AccordionContent
         content={<div>
-          Content2
+          {SECOND_CONTENT}
         </div>}
-        dataCy="content2"
+        dataCy={SECOND_CONTENT}
       />,
     },
   ];
@@ -34,33 +39,34 @@ describe("Accordion component", () => {
   it("should render the accordion and all options", () => {
     cy.mount(<Accordion items={ACCORDION_ITEMS_EXAMPLE} />);
 
-    cy.get(getDataCy("content1")).should("exist");
-    cy.get(getDataCy("content2")).should("exist");
-    cy.get(getDataCy("trigger1")).should("exist");
-    cy.get(getDataCy("trigger2")).should("exist");
+    cy.get(getDataCy(FIRST_CONTENT)).should("exist");
+    cy.get(getDataCy(SECOND_CONTENT)).should("exist");
+    cy.get(getDataCy(FIRST_TRIGGER)).should("exist");
+    cy.get(getDataCy(SECOND_TRIGGER)).should("exist");
   });
 
   it("should accordion option be closed (text hidden)", () => {
     cy.mount(<Accordion items={ACCORDION_ITEMS_EXAMPLE} />);
 
-    cy.get(getDataCy("content1")).should("be.not.visible");
+    cy.get(getDataCy(FIRST_CONTENT)).should("be.not.visible");
   });
 
   it("should accordion option be opened when click trigger", () => {
     cy.mount(<Accordion items={ACCORDION_ITEMS_EXAMPLE} />);
-    cy.get(getDataCy("trigger1")).click();
 
-    cy.get(getDataCy("content1")).should("be.visible");
+    cy.get(getDataCy(FIRST_TRIGGER)).click();
+
+    cy.get(getDataCy(FIRST_CONTENT)).should("be.visible");
   });
 
   it("should all options could be opened and closed one by one", () => {
     cy.mount(<Accordion items={ACCORDION_ITEMS_EXAMPLE} />);
 
-    cy.get(getDataCy("trigger1")).click();
-    cy.get(getDataCy("trigger2")).click();
+    cy.get(getDataCy(FIRST_TRIGGER)).click();
+    cy.get(getDataCy(SECOND_TRIGGER)).click();
 
-    cy.get(getDataCy("content1")).should("be.not.visible");
-    cy.get(getDataCy("content2")).should("be.visible");
+    cy.get(getDataCy(FIRST_CONTENT)).should("be.not.visible");
+    cy.get(getDataCy(SECOND_CONTENT)).should("be.visible");
   });
 
   it("should all options be opened and closed in multiple mode", () => {
@@ -69,11 +75,11 @@ describe("Accordion component", () => {
       type={accordionTypes.multiple}
     />);
 
-    cy.get(getDataCy("trigger1")).click();
-    cy.get(getDataCy("trigger2")).click();
+    cy.get(getDataCy(FIRST_TRIGGER)).click();
+    cy.get(getDataCy(SECOND_TRIGGER)).click();
 
-    cy.get(getDataCy("content1")).should("be.visible");
-    cy.get(getDataCy("content2")).should("be.visible");
+    cy.get(getDataCy(FIRST_CONTENT)).should("be.visible");
+    cy.get(getDataCy(SECOND_CONTENT)).should("be.visible");
   });
 
   it("should only one option be opened in not multiple mode", () => {
@@ -82,14 +88,14 @@ describe("Accordion component", () => {
       type={accordionTypes.single}
     />);
 
-    cy.get(getDataCy("trigger1")).click();
+    cy.get(getDataCy(FIRST_TRIGGER)).click();
 
-    cy.get(getDataCy("content1")).should("be.visible");
-    cy.get(getDataCy("content2")).should("be.not.visible");
+    cy.get(getDataCy(FIRST_CONTENT)).should("be.visible");
+    cy.get(getDataCy(SECOND_CONTENT)).should("be.not.visible");
 
-    cy.get(getDataCy("trigger2")).click();
+    cy.get(getDataCy(SECOND_TRIGGER)).click();
 
-    cy.get(getDataCy("content1")).should("be.not.visible");
-    cy.get(getDataCy("content2")).should("be.visible");
+    cy.get(getDataCy(FIRST_CONTENT)).should("be.not.visible");
+    cy.get(getDataCy(SECOND_CONTENT)).should("be.visible");
   });
 });
