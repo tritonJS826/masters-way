ed1d0c88f63b41d4b11b417056e20fbb5adc63e3
Ekaterina1994 <katishteno@mail.ru>
2023-11-20 14:57:25 +0300

    fix: #168 Added descrption to login comment

--- mw-webapp/src/service/auth/logIn.ts
+++ mw-webapp/src/service/auth/logIn.ts
@@ -7,18 +7,15 @@ import {auth, provider} from "src/firebase";
 export const logIn = async () => {
 
   /**
-   * TODO: IMPORTANT! Refactor required!!!
+   * TODO:
    * LogIn with signInWithPopUp and signInWithRedirect methods doesn't work in Opera.
    * Also need to check how it works in other browsers: Edge, Safari
    * Using signInWithRedirect works in Chrome but doesn't work in FireFox, Opera and maybe other browsers
    * Using signInWithPopUp works in FireFox but calls in Chrome Error:
-   * Cross-Origin-Opener-Policy policy would block the window.closed call.
-   * According
-   * https://stackoverflow.com/questions/76446840/cross-origin-opener-policy-policy-would-block-the-window-closed-call-error-while
-   * The problem is with Cross-Origin-Embedder-Policy header, which is by default should be set to unsafe-none.
-   * Also Firebase recommends the next steps to resolve this issue:
+   * "Cross-Origin-Opener-Policy policy would block the window.closed call."
+   * Resolve solutions:
    * https://firebase.google.com/docs/auth/web/redirect-best-practices
-   *
+   * https://stackoverflow.com/questions/76446840/cross-origin-opener-policy-policy-would-block-the-window-closed-call-error-while
    */
   await signInWithPopup(auth, provider);
 };
