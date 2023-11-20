77e8d74bb573f9cc295ef7410e004e02f9652a8f
Ekaterina1994 <katishteno@mail.ru>
2023-11-20 12:29:11 +0300

    docs: #168 added comment about mistake

--- mw-webapp/src/service/auth/logIn.ts
+++ mw-webapp/src/service/auth/logIn.ts
@@ -5,5 +5,16 @@ import {auth, provider} from "src/firebase";
  * Call method for log in
  */
 export const logIn = async () => {
+
+  /**
+   * TODO: IMPORTANT!
+   * Using signInWithPopUp calls Error: Cross-Origin-Opener-Policy policy would block the window.closed call.
+   * According
+   * https://stackoverflow.com/questions/76446840/cross-origin-opener-policy-policy-would-block-the-window-closed-call-error-while
+   * The problem is with Cross-Origin-Embedder-Policy header, which is by default should be set to unsafe-none.
+   * Also Firebase recommends the next steps to resolve this issue:
+   * https://firebase.google.com/docs/auth/web/redirect-best-practices
+   *
+   */
   await signInWithPopup(auth, provider);
 };
