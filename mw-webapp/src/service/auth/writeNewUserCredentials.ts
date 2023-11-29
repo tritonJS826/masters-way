c47bc2b7a9d1bfa9284f516c65ec2bed28c6265f
Ekaterina1994 <katishteno@mail.ru>
2023-11-29 19:30:44 +0300

    fix: #228 Fix

--- mw-webapp/src/service/auth/writeNewUserCredentials.ts
+++ mw-webapp/src/service/auth/writeNewUserCredentials.ts
@@ -8,17 +8,15 @@ export const writeNewUserCredentials = async (currentUser: User) => {
   if (!currentUser) {
     return;
   }
-  if (currentUser.email && currentUser.displayName) {
-    UserService.createUserDTO(
-      {
-        uuid: currentUser.uid,
-        email: currentUser.email,
-        name: currentUser.displayName,
-        description: "",
-        ownWayUuids: [],
-        favoriteWayUuids: [],
-        mentoringWayUuids: [],
-      },
-    );
-  }
+  UserService.createUserDTO(
+    {
+      uuid: currentUser.uid,
+      email: currentUser.email ?? "example@gmail.com",
+      name: currentUser.displayName ?? "Empty name",
+      description: "",
+      ownWayUuids: [],
+      favoriteWayUuids: [],
+      mentoringWayUuids: [],
+    },
+  );
 };
