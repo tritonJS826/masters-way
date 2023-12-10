80f04cf11741050c45ba43755fab235c835443fb
tritonJS826 <triton.68@mail.ru>
2023-12-10 21:24:45 +0400

    feat: #223 disable auto navigation on log in

--- mw-webapp/src/App.tsx
+++ mw-webapp/src/App.tsx
@@ -1,11 +1,9 @@
 import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
 import {Header} from "src/component/header/Header";
 import {UserPreviewDAL} from "src/dataAccessLogic/UserPreviewDAL";
 import {globalContext} from "src/GlobalContext";
 import {useErrorHandler} from "src/hooks/useErrorHandler";
 import {UserPreview} from "src/model/businessModelPreview/UserPreview";
-import {pages} from "src/router/pages";
 import {Router} from "src/router/Router";
 import {AuthService} from "src/service/AuthService";
 
@@ -14,7 +12,6 @@ import {AuthService} from "src/service/AuthService";
  */
 export const App = () => {
   useErrorHandler();
-  const navigate = useNavigate();
   const [user, setUser] = useState<UserPreview | null>(null);
 
   /**
@@ -35,12 +32,6 @@ export const App = () => {
   useEffect(() => {
     AuthService.listenAuthStateChange({onLogIn, onLogOut});
   }, []);
-
-  useEffect(() => {
-    if (user) {
-      navigate(pages.user.getPath({uuid: user?.uuid}));
-    }
-  }, [user]);
 
   return (
 
