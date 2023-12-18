4eb770b0fbbf684840e4ce008d3c8570fb8bebcd
Arthur <114860794+Howl404@users.noreply.github.com>
2023-12-18 16:56:39 +0200

    fix: #320 stop mentoring button (#321)
    * fix: #320 change modal to confirm, buttons container styles
    
    * refactor: #320 move comment, remove !!, remove class
    
    * feat: #320 ability to remove mentor on way page, move sections to separate files

---
+++ mw-webapp/src/logic/wayPage/MentorRequestsSection.tsx
@@ -0,0 +1,96 @@
+import {Button} from "src/component/button/Button";
+import {Link} from "src/component/link/Link";
+import {HeadingLevel, Title} from "src/component/title/Title";
+import {UserPreviewDAL} from "src/dataAccessLogic/UserPreviewDAL";
+import {WayDAL} from "src/dataAccessLogic/WayDAL";
+import {Way} from "src/model/businessModel/Way";
+import {UserPreview} from "src/model/businessModelPreview/UserPreview";
+import {pages} from "src/router/pages";
+
+/**
+ * Add mentor to Way
+ */
+const addMentorToWay = (
+  way: Way,
+  setWay: (newWay: Way) => void,
+  userPreview: UserPreview,
+) => {
+  const mentoringWays = userPreview.mentoringWays.concat(userPreview.uuid);
+  const newUserPreview = new UserPreview({...userPreview, mentoringWays});
+
+  UserPreviewDAL.updateUserPreview(newUserPreview);
+
+  const mentors = way.mentors.concat(newUserPreview);
+  const mentorRequests = way.mentorRequests.filter((item) => item !== userPreview);
+  const newWay = new Way({...way, mentors, mentorRequests});
+
+  WayDAL.updateWay(newWay);
+
+  setWay(newWay);
+};
+
+/**
+ * Remove user from Way's mentor requests
+ */
+const removeUserFromMentorRequests = (
+  way: Way,
+  setWay: (newWay: Way) => void,
+  userPreview: UserPreview) => {
+
+  const mentorRequests = way.mentorRequests.filter((item) => item !== userPreview);
+
+  const newWay = new Way({...way, mentorRequests});
+
+  WayDAL.updateWay(newWay);
+  setWay(newWay);
+};
+
+/**
+ * Mentor Requests Section Props
+ */
+interface MentorRequestsSectionProps {
+
+  /**
+   * Way from which mentors should be displayed
+   */
+  way: Way;
+
+  /**
+   * Function to set updated Way
+   */
+  setWay: (newWay: Way) => void;
+}
+
+/**
+ * Section with requests to become Way mentor
+ */
+export const MentorRequestsSection = (props: MentorRequestsSectionProps) => {
+  return (
+    <>
+      <Title
+        level={HeadingLevel.h3}
+        text="Mentors of this way:"
+      />
+      {props.way.mentorRequests.map((userPreview) => (
+        <div key={userPreview.uuid}>
+          <Link
+            value={userPreview.name}
+            path={pages.user.getPath({uuid: userPreview.uuid})}
+          />
+          <Button
+            value='Accept'
+            onClick={() =>
+              addMentorToWay(props.way, props.setWay, userPreview)
+            }
+          />
+          <Button
+            value='Decline'
+            onClick={() =>
+              removeUserFromMentorRequests(props.way, props.setWay, userPreview)
+            }
+          />
+        </div>
+      ))}
+    </>
+  );
+};
