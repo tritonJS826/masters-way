c7572c515f079e3fd1a1d3003e998a81e68877cd
Dayana <dayana.shakenova20@gmail.com>
2023-12-27 10:20:57 +0600

    feat: #340 add new migration deleteFavoritesInWaysAndUsers (#341)
    * feat: #340 add new migration
    
    * fix: #340 resolve conversation
    
    * fix: #340 update file names
    
    * refactor: #340 rename UserService

---
+++ mw-migrations/src/migrations/usersWaysMigrations-delete-favorites.ts
@@ -0,0 +1,38 @@
+import { writeBatch } from "firebase/firestore";
+import { db } from "../firebase.js";
+import { logToFile } from "../utils/logToFile.js";
+import { updateUsersFavorites } from "./usersMigration-update-favorites.js";
+import { updateWaysFavorites } from "./waysMigration-update-favorites.js";
+
+const FILE_TO_LOG = "usersMigration_delete_favoriteWayUuids_waysMigration_delete_favoriteForUserUuids"
+const log = (textToLog: string) => logToFile(`${(new Date()).toISOString()}: ${textToLog}`, FILE_TO_LOG);
+
+const deleteFavoritesInWaysAndUsers = async () => {
+  const usersAndWaysMigrationStartTime = new Date();
+  log(`Migration started`);
+
+  const batch = writeBatch(db);
+
+  const amountOfUsers = await updateUsersFavorites(batch, log);
+  const amountOfWays = await updateWaysFavorites(batch, log);
+
+  await batch.commit();
+
+  const usersAndWaysMigrationEndTime = new Date();
+  const usersAndWaysMigrationTime = usersAndWaysMigrationEndTime.getTime() - usersAndWaysMigrationStartTime.getTime();
+
+  log(`
+    Migrations report:
+
+    Migration goal:
+    Delete favorites for users and ways
+    
+    Start time: ${usersAndWaysMigrationStartTime}
+    End time: ${usersAndWaysMigrationEndTime}
+    Total time: ${usersAndWaysMigrationTime} ms
+
+    Total Models to changed: Users - ${amountOfUsers}, Ways - ${amountOfWays}
+  `)
+}
+
+deleteFavoritesInWaysAndUsers()
---
+++ mw-migrations/src/service/UserService.ts
@@ -0,0 +1,24 @@
+import { collection, getDocs } from "firebase/firestore";
+import { querySnapshotToDTOConverter } from "../converter/querySnapshotToDTOConverter.js";
+import { db } from "../firebase.js";
+import { UserDTOMigration } from "../DTOModel/UserDTO.js";
+
+export const PATH_TO_USERS_COLLECTION = "users";
+
+/**
+ * Provides methods to interact with the Users collection in Firestore.
+ */
+export class UserService {
+
+  /**
+   * Get UsersDTO
+   */
+  public static async getUsersDTO(): Promise<UserDTOMigration[]> {
+    const usersRef = collection(db, PATH_TO_USERS_COLLECTION);
+    const usersRaw = await getDocs(usersRef);
+    const usersDTO = querySnapshotToDTOConverter<UserDTOMigration>(usersRaw);
+
+    return usersDTO;
+  }
+
+}
