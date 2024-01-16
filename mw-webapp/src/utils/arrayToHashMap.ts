f01ab4a4a256d0d773f521cf35044064ed445090
Ekaterina1994 <katishteno@mail.ru>
2024-01-16 21:44:15 +0300

    fix: #360 Fix

--- mw-webapp/src/utils/arrayToHashMap.ts
+++ mw-webapp/src/utils/arrayToHashMap.ts
@@ -1,25 +1,28 @@
 /**
  * ArrayToHashMapArgs
  */
-interface ArrayToHashMapArgs<Entity> {
+interface ArrayToHashMapArgs<Entity extends object, Key extends keyof Entity> {
 
   /**
    * Field of {@link Entity} that will be used as key for hashmap
    */
-  keyField: keyof Entity;
+  keyField: Key;
 
   /**
    * Array's values that will be converted to hashmap
    */
   list: Entity[];
-
 }
 
 /**
  * Create hashmap from array
  */
-export const arrayToHashMap = <Entity>(args: ArrayToHashMapArgs<Entity>): Map<string, Entity> => {
-  const mappedList = args.list.map((item: Entity): [string, Entity] => [String(item[args.keyField]), item]);
+export const arrayToHashMap = <
+  Entity extends object,
+  Key extends keyof Entity
+>(args: ArrayToHashMapArgs<Entity, Key>): Map<Entity[Key], Entity> => {
+
+  const mappedList = args.list.map((item: Entity): [Entity[Key], Entity] => [item[args.keyField], item]);
   const hashMap = new Map(mappedList);
 
   return hashMap;
