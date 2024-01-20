7e927c6aa4fd6e1d76ce05f1cf33ace8d7ea0ed3
Ekaterina1994 <katishteno@mail.ru>
2024-01-20 17:59:25 +0300

    fix: #335 Added component Confirm to storybook

---
+++ mw-webapp/src/component/confirm/Confirm.stories.tsx
@@ -0,0 +1,45 @@
+import {useState} from "react";
+import type {StoryFn} from "@storybook/react";
+import {Button} from "src/component/button/Button";
+import {Confirm} from "src/component/confirm/Confirm";
+
+const meta = {
+  title: "Confirm",
+  component: Confirm,
+  parameters: {layout: "centered"},
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+const defaultContent = (
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+  </div>
+);
+
+const Template: StoryFn<typeof Confirm> = (args) => {
+  const [isOpen, setIsOpen] = useState(args.isOpen);
+
+  return (
+    <Confirm
+      trigger={
+        <Button
+          value="Click me!"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        />}
+      content={defaultContent}
+      isOpen={isOpen}
+      onOk={() => {}}
+      okText="Ok"
+    />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {isOpen: false};
+
+export const Opened = Template.bind({});
+Opened.args = {isOpen: true};
