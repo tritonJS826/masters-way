5e422ed006ccc76c13b98072d7ba0f12a9ba6676
Alex89198900 <alexchorn1989@ukr.net>
2024-01-22 19:13:04 +0200

    fix: #9 correct variables

--- mw-webapp/src/component/progressBar/ProgressBar.tsx
+++ mw-webapp/src/component/progressBar/ProgressBar.tsx
@@ -3,31 +3,27 @@ import styles from "src/component/progressBar/ProgressBar.module.scss";
                            * ProgressBar prop type
                            */
    progress: Array<boolean>;}
+const MAX_PERCENTAGE = 100;
 
 /**
  * ProgressBar component
  */
 export const ProgressBar = (arg: ProgressBarProps) => {
-  const PERCENT = 100;
   const countTrue = arg.progress.filter((item) => item).length;
-  const computedProcent = (PERCENT / arg.progress.length) * countTrue;
+  const computedProcent = (MAX_PERCENTAGE / arg.progress.length) * countTrue;
   if (arg.progress.length > 0) {
     return (
-      <>
-        <div className={styles.progressBarContainer}>
-          <div
-            style={{width: `${computedProcent}%`}}
-            className={styles.progressIndicator}
-          >
-            <div className={styles.progressIndicatorPercent}>
-              {computedProcent.toFixed() + "%"}
-            </div>
+      <div className={styles.progressBarContainer}>
+        <div
+          style={{width: `${computedProcent}%`}}
+          className={styles.progressIndicator}
+        >
+          <div className={styles.progressIndicatorPercent}>
+            {computedProcent.toFixed() + "%"}
           </div>
-
         </div>
 
-      </>
-
+      </div>
     );
   }
 };
