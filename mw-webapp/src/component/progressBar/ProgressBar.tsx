06fd1eb2d288c93baa8686f6ba40e60bff3e8397
Alex89198900 <alexchorn1989@ukr.net>
2024-01-22 20:20:40 +0200

    fix: #11 style

--- mw-webapp/src/component/progressBar/ProgressBar.tsx
+++ mw-webapp/src/component/progressBar/ProgressBar.tsx
@@ -1,17 +1,25 @@
 import styles from "src/component/progressBar/ProgressBar.module.scss";
- type ProgressBarProps = {/**
-                           * ProgressBar prop type
-                           */
-   progress: Array<boolean>;}
+
 const MAX_PERCENTAGE = 100;
+
+/**
+ * Type props
+ */
+interface ProgressBarProps {
+
+  /**
+   * Array of metrics
+   */
+  progress: boolean[];
+}
 
 /**
  * ProgressBar component
  */
-export const ProgressBar = (arg: ProgressBarProps) => {
-  const countTrue = arg.progress.filter((item) => item).length;
-  const computedProcent = (MAX_PERCENTAGE / arg.progress.length) * countTrue;
-  if (arg.progress.length > 0) {
+export const ProgressBar = (props: ProgressBarProps) => {
+  const countTrue = props.progress.filter((item) => item).length;
+  const computedProcent = (MAX_PERCENTAGE / props.progress.length) * countTrue;
+  if (props.progress.length > 0) {
     return (
       <div className={styles.progressBarContainer}>
         <div
