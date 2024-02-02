545241c1953e43132a2a9b443d4c40ae6ef040b4
Ekaterina <57002521+Ekaterina1994@users.noreply.github.com>
2024-02-02 17:14:47 +0300

    fix: #390 Deleted clear goals and metrics (#491)

--- mw-migrations/src/backup/backupHelpers/clearAllCollections.ts
+++ mw-migrations/src/backup/backupHelpers/clearAllCollections.ts
@@ -1,6 +1,4 @@
 import { DayReportService } from "../../service/DayReportService.js";
-import { GoalMetricService } from "../../service/GoalMetricService.js";
-import { GoalService } from "../../service/GoalService.js";
 import { UserService } from "../../service/UserService.js";
 import { WayService } from "../../service/WayService.js";
 import { logToFile } from "../../utils/logToFile.js";
@@ -26,31 +24,17 @@ export const clearAllCollections = async () => {
   log(`Got ${allDayReports.length} dayReports`)
   log(`Required ${allDayReports.length} READ operations`)
 
-  log(`Getting Goals collection`)
-  const allGoals = await GoalService.getGoalsDTO();
-  log(`Got ${allGoals.length} goals`)
-  log(`Required ${allGoals.length} READ operations`)
-
-  log(`Getting GoalMetrics collection`)
-  const allGoalMetrics = await GoalMetricService.getGoalMetricsDTO();
-  log(`Got ${allGoalMetrics.length} goalMetrics`)
-  log(`Required ${allGoalMetrics.length} READ operations`)
-
-  const totalReadOperationsAmount = allUsers.length + allWays.length + allDayReports.length + allGoals.length + allGoalMetrics.length;
+  const totalReadOperationsAmount = allUsers.length + allWays.length + allDayReports.length;
 
   log(`Deleting documents in collections`)
   const deleteUsersPromise = allUsers.map(user => user.uuid).map(UserService.deleteUserDTO);
   const deleteWaysPromise = allWays.map(way => way.uuid).map(WayService.deleteWayDTO);
   const deleteDayReportsPromise = allDayReports.map(report => report.uuid).map(DayReportService.deleteDayReportDTO);
-  const deleteGoalsPromise = allGoals.map(goal => goal.uuid).map(GoalService.deleteGoalDTO);
-  const deleteGoalMetricsPromise = allGoalMetrics.map(goalMetric => goalMetric.uuid).map(GoalMetricService.deleteGoalMetricsDTO);
 
   const { length: deletedAmount } = await Promise.all([
     ...deleteUsersPromise,
     ...deleteWaysPromise,
     ...deleteDayReportsPromise,
-    ...deleteGoalsPromise,
-    ...deleteGoalMetricsPromise,
   ])
   log(`Deleting documents in collections finished`)
 
