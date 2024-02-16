27727272748c9c1a3e62f39e80b8771e433e43e2
Ekaterina1994 <katishteno@mail.ru>
2024-02-16 11:04:57 +0300

    fix: #419 fix

--- mw-webapp/src/component/input/formatters.ts
+++ mw-webapp/src/component/input/formatters.ts
@@ -7,14 +7,7 @@ export class FormatterInputValue {
    * Delete first zero from input number
    */
   public static withNoFirstZero(value: number) {
-    const SECOND_INDEX = 1;
-    const valueStringified = value.toString();
-
-    const formattedValue = valueStringified.startsWith("0")
-      ? valueStringified.slice(SECOND_INDEX)
-      : value;
-
-    return formattedValue;
+    return String(value);
   }
 
   /**
