a8e79f815c2217a884eac9226c119a80fe8affeb
Viktar Veratsennikau <56690686+tritonJS826@users.noreply.github.com>
2024-02-26 13:24:11 +0400

    feat: #586 added first e2e test (#587)

--- mw-webapp/cypress.config.ts
+++ mw-webapp/cypress.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
     devServer: {
       framework: "react",
       bundler: "vite",
-    }
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
