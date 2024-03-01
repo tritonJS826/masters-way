d087c5cc5cb3642221040129fa8f5333128c441f
Ekaterina1994 <katishteno@mail.ru>
2024-03-01 10:59:28 +0300

    fix: #188 Fix

--- mw-webapp/src/component/notification/Notification.cy.tsx
+++ mw-webapp/src/component/notification/Notification.cy.tsx
@@ -2,6 +2,10 @@ import {Button} from "src/component/button/Button";
 import {displayNotification} from "src/component/notification/displayNotification";
 import {getDataCy} from "src/utils/cyTesting/getDataCy";
 
+// We using className for get notification because we can't add dataCy to element created by toastify
+const NOTIFICATION = ".toastify";
+const NOTIFICATION_CLOSE_BUTTON = ".toast-close";
+const NOTIFICATION_DURATION = 1000;
 const NOTIFICATION_TEXT = "Notification text";
 const NOTIFICATION_TYPE = "info";
 const NOTIFICATION_TRIGGER_CY = "button";
@@ -13,7 +17,7 @@ const NotificationTest = () => {
   return (
     <Button
       value="Notification trigger"
-      onClick={() => displayNotification({text: NOTIFICATION_TEXT, type: NOTIFICATION_TYPE})}
+      onClick={() => displayNotification({text: NOTIFICATION_TEXT, type: NOTIFICATION_TYPE, duration: NOTIFICATION_DURATION})}
       dataCy={NOTIFICATION_TRIGGER_CY}
     />
   );
@@ -28,25 +32,37 @@ describe("Notification component", () => {
     );
   });
 
-  it("should render on trigger", () => {
-    cy.get(".toastify").should("not.exist");
-    cy.get(getDataCy(NOTIFICATION_TRIGGER_CY)).click();
-    cy.get(".toastify").should("exist");
-  });
-
-  it("should render right content", () => {
-    cy.get(".toastify").should("contain.text", NOTIFICATION_TEXT);
-  });
-
-  it("should close button exist", () => {
-    cy.get(".toastify").children().last().invoke("prop", "tagName")
-      .then($tag => $tag.toLowerCase())
-      .should("eq", "button");
+  it(`should render on trigger and automatically hide after ${NOTIFICATION_DURATION} ms`, () => {
+    cy.get(NOTIFICATION)
+      .should("not.exist");
+    cy.get(getDataCy(NOTIFICATION_TRIGGER_CY))
+      .click();
+    cy.get(NOTIFICATION)
+      .should("exist");
+    cy.get(NOTIFICATION)
+      .should("not.exist");
   });
 
   it("should notification hide by clicking on close button", () => {
-    cy.get(".toastify").children().last().click();
-    cy.get(".toastify").should("not.exist");
+    cy.get(getDataCy(NOTIFICATION_TRIGGER_CY))
+      .click();
+    cy.get(NOTIFICATION)
+      .find(NOTIFICATION_CLOSE_BUTTON)
+      .should("exist")
+      .click();
+    cy.get(NOTIFICATION)
+      .should("not.exist");
+  });
+
+  it("should render right content", () => {
+    cy.get(getDataCy(NOTIFICATION_TRIGGER_CY))
+      .click();
+    cy.get(NOTIFICATION)
+      .should("contain.text", NOTIFICATION_TEXT);
+    cy.get(NOTIFICATION)
+      .find(NOTIFICATION_CLOSE_BUTTON)
+      .should("exist")
+      .click();
   });
 
 });
--- mw-webapp/src/component/select/Select.cy.tsx
+++ mw-webapp/src/component/select/Select.cy.tsx
@@ -2,30 +2,26 @@ import {useState} from "react";
 import {Select} from "src/component/select/Select";
 import {getDataCy} from "src/utils/cyTesting/getDataCy";
 
-const SELECT_LABEL = "Select label";
-const SELECT_NAME = "selectName";
 const SELECT_CY = "select";
-const TEXT_ONE = "Select text 1";
-const TEXT_TWO = "Select text 2";
-const VALUE_ONE = "value 1";
-const VALUE_TWO = "value 2";
+const FIRST_OPTION_INDEX = 0;
+const SECOND_OPTION_INDEX = 1;
 const SELECT_OPTIONS = [
-  {id: "1", value: VALUE_ONE, text: TEXT_ONE},
-  {id: "2", value: VALUE_TWO, text: TEXT_TWO},
+  {id: "1", value: "value 1", text: "Select text 1"},
+  {id: "2", value: "value 2", text: "Select text 2"},
 ];
 
 /**
  * Select for test
  */
 const SelectTest = () => {
-  const [value, setValue] = useState(SELECT_OPTIONS[0].value);
+  const [value, setValue] = useState(SELECT_OPTIONS[FIRST_OPTION_INDEX].value);
 
   return (
     <Select
       dataCy={SELECT_CY}
-      label={SELECT_LABEL}
+      label="Select label"
       value={value}
-      name={SELECT_NAME}
+      name="selectName"
       options={SELECT_OPTIONS}
       onChange={setValue}
     />
@@ -42,17 +38,27 @@ describe("Select component", () => {
   });
 
   it("should render select and all options", () => {
-    cy.get(getDataCy(SELECT_CY)).should("exist")
-      .find("option").should("have.length", SELECT_OPTIONS.length);
+    cy.get(getDataCy(SELECT_CY))
+      .should("exist")
+      .find("option")
+      .should("have.length", SELECT_OPTIONS.length);
   });
 
   it("should selected value be visible", () => {
-    cy.get(getDataCy(SELECT_CY)).find("option:selected").should("be.visible");
+    cy.get(getDataCy(SELECT_CY))
+      .find("option:selected")
+      .should("be.visible");
   });
 
   it("should trigger onClick if some option selected", () => {
-    cy.get(getDataCy(SELECT_CY)).find("option:selected").should("contain.text", TEXT_ONE);
-    cy.get(getDataCy(SELECT_CY)).select(VALUE_TWO).trigger("change").find("option:selected").should("contain.text", TEXT_TWO);
+    cy.get(getDataCy(SELECT_CY))
+      .find("option:selected")
+      .should("contain.text", SELECT_OPTIONS[FIRST_OPTION_INDEX].text);
+    cy.get(getDataCy(SELECT_CY))
+      .select(SELECT_OPTIONS[SECOND_OPTION_INDEX].value)
+      .trigger("change")
+      .find("option:selected")
+      .should("contain.text", SELECT_OPTIONS[SECOND_OPTION_INDEX].text);
   });
 
 });
