e84055b04f9c86aebdfdb0b2194c2c17fb98b34f
tritonJS826 <triton.68@mail.ru>
2024-03-20 11:24:44 +0400

    fix: #622 migration from firebase to pg

--- mw-migrations/migrateToPostgres/migrate.ts
+++ mw-migrations/migrateToPostgres/migrate.ts
@@ -11,6 +11,15 @@ import { dayReports } from './migrateTable/day_reports.js';
 import { favorite_users } from './migrateTable/favorite_users.js';
 import { jobTags } from './migrateTable/job_tags.js';
 import { wayCollections } from './migrateTable/way_collections.js';
+import { to_user_mentoring_requests } from './migrateTable/to_user_mentoring_requests.js';
+import { from_user_mentoring_request } from './migrateTable/from_user_mentoring_requests.js';
+import { favorite_users_ways } from './migrateTable/favorite_users_ways.js';
+import { former_mentors_ways } from './migrateTable/former_mentors_ways.js';
+import { mentor_users_ways } from './migrateTable/mentor_users_ways.js';
+import { comments } from './migrateTable/comments.js';
+import { problems } from './migrateTable/problems.js';
+import { job_dones } from './migrateTable/job_dones.js';
+import { plans } from './migrateTable/plans.js';
 
 const { Client } = pkg;
 
@@ -36,21 +45,35 @@ async function startMigration() {
     ways_way_tags();
     way_tags();
     favorite_users();
-
-    // users(client);
-    // await delay(2_000)
-    // ways(client);
-    // await delay(2_000)
-    // metrics(client)
-    // await delay(2_000)
-    // dayReports(client)
-    // await delay(2_000)
-    // jobTags(client)
-    // await delay(2_000)
+    to_user_mentoring_requests();
+    former_mentors_ways
+    users(client);
+    await delay(2_000)
+    ways(client);
+    await delay(2_000)
+    metrics(client)
+    await delay(2_000)
+    dayReports(client)
+    await delay(2_000)
+    jobTags(client)
+    await delay(2_000)
     await wayCollections(client) // + way_collections_ways
     await delay(2_000)
-
-    // 12/22 done
+    from_user_mentoring_request(client)
+    await delay(2_000)
+    favorite_users_ways(client)
+    await delay(2_000)
+    former_mentors_ways(client)
+    await delay(2_000)
+    mentor_users_ways(client)
+    await delay(2_000)
+    comments(client)
+    await delay(2_000)
+    problems(client) // with problems_job_tags
+    await delay(2_000)
+    job_dones(client) // with job_dones_job_tags
+    await delay(2_000)
+    plans(client) // with job_dones_job_tags
 
     // await client.end()
     // .then(() => console.log('Connection to PostgreSQL closed'))
