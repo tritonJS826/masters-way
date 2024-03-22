fc6233f053c47bf756f0340baa681d20c0e3a12f
AKiroi <alexandr.kiroi@gmail.com>
2024-03-22 22:21:11 +0200

    feat: #188 add tests to loader

--- mw-webapp/src/component/loader/Loader.tsx
+++ mw-webapp/src/component/loader/Loader.tsx
@@ -3,12 +3,26 @@ import {Image} from "src/component/image/Image";
 import styles from "src/component/loader/Loader.module.scss";
 
 /**
+ * Props for the Image component
+ */
+interface LoaderProps {
+
+  /**
+   * Data attribute for cypress testing
+   */
+  dataCy?: string;
+}
+
+/**
  * Loader component
  * Used for page loading
  */
-export const Loader = () => {
+export const Loader = (props: LoaderProps) => {
   return (
-    <div className={styles.loaderWrapper}>
+    <div
+      className={styles.loaderWrapper}
+      data-cy={props.dataCy}
+    >
       <Image
         src={logo}
         alt="Loading image"
