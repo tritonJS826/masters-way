4712c4582309bc8fb0b9ac5e02684df9be703a28
Alex89198900 <alexchorn1989@ukr.net>
2024-04-01 20:03:52 +0300

    fix: #188 tests editable text

--- mw-webapp/src/component/editableText/EditableText.cy.tsx
+++ mw-webapp/src/component/editableText/EditableText.cy.tsx
@@ -62,21 +62,33 @@ describe("EditableText component", () => {
     cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("have.attr", "placeholder", EDITABLETEXTINPUT_PLACEHOLDER);
   });
 
-  it("shoud be works with EditableText mode number", () => {
+  it("shoud work with mode 'number'", () => {
     cy.mount(createTestEditableText({value: EDITABLETEXTINPUT_VALUE_NUMBER, type: InputType.Number}));
     cy.get(getDataCy(EDITABLETEXT_CY.trigger))
       .dblclick();
-    cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("have.attr", "type", InputType.Number);
+    cy.get(getDataCy(EDITABLETEXTINPUT_CY)).type("now");
     cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("have.value", EDITABLETEXTINPUT_VALUE_NUMBER);
   });
 
-  it("shoud be render input or span depend on client actions", () => {
+  it("shoud render input or div depend on client actions", () => {
     cy.mount(createTestEditableText({value: EDITABLETEXTINPUT_VALUE, type: InputType.Text}));
     cy.get(getDataCy(EDITABLETEXT_CY.trigger))
       .dblclick();
-    cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("have.attr", "placeholder", EDITABLETEXTINPUT_PLACEHOLDER);
+    cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("have.value", EDITABLETEXTINPUT_VALUE);
     cy.get(BODY_DOCUMENT).click(COORDINATES_BODY, COORDINATES_BODY, {force: true});
     cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("not.exist");
+    cy.get(getDataCy(EDITABLETEXT_CY.trigger)).find("div").should("have.text", EDITABLETEXTINPUT_VALUE);
+  });
+
+  it("shoud be render div after change finished", () => {
+    cy.mount(createTestEditableText({value: EDITABLETEXTINPUT_VALUE, type: InputType.Text}));
+    cy.get(getDataCy(EDITABLETEXT_CY.trigger))
+      .dblclick();
+    cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("have.value", EDITABLETEXTINPUT_VALUE);
+    cy.get(getDataCy(EDITABLETEXT_CY.trigger))
+      .should("exist").trigger("keydown", {key: "Enter"});
+    cy.get(getDataCy(EDITABLETEXTINPUT_CY)).should("not.exist");
+    cy.get(getDataCy(EDITABLETEXT_CY.trigger)).find("div").should("have.text", EDITABLETEXTINPUT_VALUE);
   });
 
 });
