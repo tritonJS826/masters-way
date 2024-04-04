60d39919ba62df7bd6de1efcaed72e9e09663a36
Ekaterina1994 <katishteno@mail.ru>
2024-04-04 13:43:38 +0300

    fix: #622 fix date in migrate

--- mw-migrations/migrateToPostgres/utils.ts
+++ mw-migrations/migrateToPostgres/utils.ts
@@ -4,11 +4,40 @@ import crypto from 'crypto';
  * TODO: update
  * it's just for example, you can use your function I didn't find it
  */
-export const firebaseDateToPgDate = (fbDate: {seconds: number, nanoseconds: number}) => {
-    return "2024-03-09 19:25:46.557"
+export const firebaseDateToPgDate = (fbDate: { seconds: number, nanoseconds: number }) => {
+  const timestamp = fbDate.seconds + fbDate.nanoseconds * 1e-9 ;
+  const dateTime = new Date(timestamp * 1000);
+
+  const year = dateTime.getFullYear().toString().padStart(4, '0');
+  const month = (dateTime.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
+  const day = dateTime.getDate().toString().padStart(2, '0');
+  const hours = dateTime.getHours().toString().padStart(2, '0');
+  const minutes = dateTime.getMinutes().toString().padStart(2, '0');
+  const seconds = dateTime.getSeconds().toString().padStart(2, '0');
+  const milliseconds = dateTime.getMilliseconds().toString().padStart(3, '0'); // Pad milliseconds
+
+
+  const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+  return formattedDateTime;
 }
 
-export const timestampToPgDate = (timestamp: number) => "2024-03-09 19:25:46.557";
+export const timestampToPgDate = (timestamp: number) => {
+  const dateTime = new Date(timestamp * 1000);
+
+  const year = dateTime.getFullYear().toString().padStart(4, '0');
+  const month = (dateTime.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
+  const day = dateTime.getDate().toString().padStart(2, '0');
+  const hours = dateTime.getHours().toString().padStart(2, '0');
+  const minutes = dateTime.getMinutes().toString().padStart(2, '0');
+  const seconds = dateTime.getSeconds().toString().padStart(2, '0');
+  const milliseconds = dateTime.getMilliseconds().toString().padStart(3, '0'); // Pad milliseconds
+
+
+  const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+  return formattedDateTime;
+};
 
 export function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
