f1b588ac2b07639f544c1ba5eb10d67b493f7a76
AKiroi <alexandr.kiroi@gmail.com>
2024-04-30 20:58:05 +0300

    feat: #666 create themeMembers

---
+++ mw-webapp/src/logic/aboutProjectPage/TeamMember/teamMembers.ts
@@ -0,0 +1,35 @@
+
+import {TeamMemberType} from "src/logic/aboutProjectPage/TeamMember/TeamMember";
+
+export const teamMembers: TeamMemberType[] = [
+  {
+    id: 1,
+    name: "Viktar Veratsennikau",
+    description: "Founder",
+    imageUrl: "",
+  },
+  {
+    id: 2,
+    name: "Ekaterina Veretennikova",
+    description: "Team Leader",
+    imageUrl: "",
+  },
+  {
+    id: 3,
+    name: "Sergei Aslanov",
+    description: "developer",
+    imageUrl: "",
+  },
+  {
+    id: 4,
+    name: "Marat Assimbayev",
+    description: "developer",
+    imageUrl: "",
+  },
+  {
+    id: 5,
+    name: "Alexandr Chorniy",
+    description: "developer",
+    imageUrl: "",
+  },
+];
