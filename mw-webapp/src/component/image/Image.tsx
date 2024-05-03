065f304849e5c76140bae468b0846d6ee8a1f66d
DenisOle <d_sudnikov@mail.ru>
2024-05-03 19:05:16 +0200

    feat: #396 added option to open Image in full screen

--- mw-webapp/src/component/image/Image.tsx
+++ mw-webapp/src/component/image/Image.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import clsx from "clsx";
 import styles from "src/component/image/Image.module.scss";
 
@@ -25,21 +26,41 @@ interface ImageProps {
    * Data attribute for cypress testing
    */
   dataCy?: string;
+
+  /**
+   * Should image open?
+   */
+  isShouldOpen?: boolean;
 }
 
 /**
  * Сomponent for displaying images
  */
 export const Image = (props: ImageProps) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   const className = clsx(styles.image, props.className);
 
+  /**
+   * Function full screen image
+   */
+  const onClickHandler = (value: boolean) => {
+    setIsOpen(value);
+  };
+
   return (
-    <img
-      src={props.src}
-      alt={props.alt}
-      className={className}
-      data-cy={props.dataCy}
-    />
+    <div className={isOpen ? styles.imageWrapperFullScreen : ""}>
+      <img
+        tabIndex={0}
+        src={props.src}
+        alt={props.alt}
+        className={className}
+        data-cy={props.dataCy}
+        onClick={props.isShouldOpen ? () => onClickHandler(true) : undefined}
+        onBlur={() => onClickHandler(false)}
+      />
+    </div>
   );
 };
 
