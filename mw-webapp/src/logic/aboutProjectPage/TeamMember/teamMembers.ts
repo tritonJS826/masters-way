6f40a6c371d386d8461a9007f9a246196ea26fd2
tritonJS826 <triton.68@mail.ru>
2024-05-13 12:26:33 +0400

    fix: #666 about team design minor fixes

--- mw-webapp/src/logic/aboutProjectPage/TeamMember/teamMembers.ts
+++ mw-webapp/src/logic/aboutProjectPage/TeamMember/teamMembers.ts
@@ -3,33 +3,27 @@ import {TeamMemberType} from "src/logic/aboutProjectPage/TeamMember/TeamMember";
 
 export const teamMembers: TeamMemberType[] = [
   {
-    id: 1,
+    id: 0,
     name: "Viktar Veratsennikau",
     description: "Founder",
     imageUrl: "https://lh3.google.com/u/0/d/18oHI9KoiaYvd_UowHyqsJbDLLhmuxPxr=w1919-h1079-iv2",
   },
   {
-    id: 2,
+    id: 1,
     name: "Ekaterina Veretennikova",
-    description: "Team Leader",
+    description: "Front-end lead",
     imageUrl: "https://lh3.google.com/u/0/d/1XTHKtc-5_TEK823s5wGVj9TYjvmvW4GW=w2000-h1826-iv2",
   },
   {
+    id: 2,
+    name: "Ekaterina Sokolova",
+    description: "QA Engineer",
+    imageUrl: "https://lh3.google.com/u/0/d/1SC7y2CUX7jRYR6VVWoy4polIO0UZZ-i8=w2000-h1826-iv1",
+  },
+  {
     id: 3,
-    name: "Sergei Aslanov",
-    description: "developer",
-    imageUrl: "",
-  },
-  {
-    id: 4,
-    name: "Marat Assimbayev",
-    description: "developer",
-    imageUrl: "",
-  },
-  {
-    id: 5,
-    name: "Alexandr Chorniy",
-    description: "developer",
-    imageUrl: "",
+    name: "Alexander Chorniy",
+    description: "Front-end dev",
+    imageUrl: "https://lh3.google.com/u/0/d/1jpszNTWpzOnndozD2sQhgvmRWXo6ky6z=w1916-h910-iv1",
   },
 ];
