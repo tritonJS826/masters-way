e26624353fc7157027848e0323f2267c2d1af17b
Ekaterina1994 <katishteno@mail.ru>
2024-05-19 23:46:16 +0300

    fix: #884 Fix few issues 884, 879, 881, 793

--- mw-webapp/src/component/chart/AreaChart.tsx
+++ mw-webapp/src/component/chart/AreaChart.tsx
@@ -135,7 +135,8 @@ export const AreaChart = observer((props: AreaChartProps) => {
    * Now it works even without Memo because of global context.
    * After migration to some state manager this line will help us to avoid bugs
    */
-  const optionsMemoized = useMemo(() => getOptions(LanguageService.way.statisticsBlock.timeSpentByDay[language]), [theme]);
+  const optionsMemoized = useMemo(() =>
+    getOptions(LanguageService.way.statisticsBlock.timeSpentByDay[language]), [theme, language]);
 
   return (
     <Line
