0291b293097ea995093f1b77e9e47b06a186a3a3
impiia <impiia28@gmail.com>
2024-05-22 01:41:58 +0400

    Modified the Image component by adding the 'zoomable' prop with an onClick method that triggers a portal with an enlarged image

--- mw-webapp/src/component/image/Image.tsx
+++ mw-webapp/src/component/image/Image.tsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+import {createPortal} from "react-dom";
 import clsx from "clsx";
 import styles from "src/component/image/Image.module.scss";
 
@@ -25,21 +27,65 @@ interface ImageProps {
    * Data attribute for cypress testing
    */
   dataCy?: string;
+
+  /**
+   * Enable image zooming on click
+   */
+  zoomable?: boolean;
 }
 
 /**
- * Сomponent for displaying images
+ * Component for displaying images
  */
 export const Image = (props: ImageProps) => {
-  const className = clsx(styles.image, props.className);
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  /**
+   * Handles the click event.
+   */
+  const handleClick = () => {
+    if (props.zoomable) {
+      setIsZoomed(!isZoomed);
+    }
+  };
+
+  const className = clsx(styles.image, props.className, props.zoomable && styles.zoomable);
+
+  /**
+   * Creates a portal for displaying a zoomed image overlay.
+   * If `isZoomed` is true, it renders an overlay with the zoomed image.
+   * Otherwise, it returns null.
+   */
+  const createZoomedPortal = () => {
+    if (isZoomed) {
+      return createPortal(
+        <div
+          className={styles.zoomedOverlay}
+          onClick={handleClick}
+        >
+          <img
+            src={props.src}
+            alt={props.alt}
+            className={styles.zoomedImage}
+          />
+        </div>,
+        document.body,
+      );
+    }
+
+    return null;
+  };
 
   return (
-    <img
-      src={props.src}
-      alt={props.alt}
-      className={className}
-      data-cy={props.dataCy}
-    />
+    <>
+      <img
+        src={props.src}
+        alt={props.alt}
+        className={className}
+        data-cy={props.dataCy}
+        onClick={handleClick}
+      />
+      {createZoomedPortal()}
+    </>
   );
 };
-
