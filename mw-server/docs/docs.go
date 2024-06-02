5c7da587802fc8daed7a2c593a2eef8e05af0ada
Viktar Veratsennikau <56690686+tritonJS826@users.noreply.github.com>
2024-06-02 17:31:21 +0400

    feat: #693 added swagger doc for children uuids (#982)

--- mw-server/docs/docs.go
+++ mw-server/docs/docs.go
@@ -3269,6 +3269,7 @@ const docTemplate = `{
         "schemas.WayPlainResponse": {
             "type": "object",
             "required": [
+                "childrenUuids",
                 "copiedFromWayUuid",
                 "createdAt",
                 "dayReportsAmount",
@@ -3287,6 +3288,12 @@ const docTemplate = `{
                 "wayTags"
             ],
             "properties": {
+                "childrenUuids": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
                 "copiedFromWayUuid": {
                     "type": "string",
                     "x-nullable": true
