f71a8801b537a22280bedb2f938f212b3d120f9e
tritonJS826 <triton.68@mail.ru>
2024-06-09 21:39:09 +0400

    Revert "Mw 848/oauth (#1028)"
    This reverts commit 5f68a88871545dacff5b81115cdbdb7db2a515ee.

--- mw-server/util/create.go
+++ mw-server/util/create.go
@@ -1,10 +1,6 @@
-package config
+package util
 
-import (
-	"log"
-
-	"github.com/spf13/viper"
-)
+import "github.com/spf13/viper"
 
 type Config struct {
 	DbDriver          string `mapstructure:"DB_DRIVER"`
@@ -17,9 +13,6 @@ type Config struct {
 	GooglClientId     string `mapstructure:"GOOGLE_CLIENT_ID"`
 	GooglClientSecret string `mapstructure:"GOOGLE_SECRET_ID"`
 	SecretSessionKey  string `mapstructure:"SECRET_SESSION_KEY"`
-	ApiBaseUrl        string `mapstructure:"API_BASE_URL"`
-	WebappBaseUrl     string `mapstructure:"WEBAPP_BASE_URL"`
-	Domain            string `mapstructure:"WEBAPP_DOMAIN"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
@@ -30,15 +23,9 @@ func LoadConfig(path string) (config Config, err error) {
 
 	err = viper.ReadInConfig()
 	if err != nil {
-		log.Fatalf("could not loadconfig: %v", err)
 		return
 	}
 
 	err = viper.Unmarshal(&config)
-	if err != nil {
-		log.Fatalf("could not loadconfig: %v", err)
-	}
 	return
 }
-
-var Env, _ = LoadConfig(".")
