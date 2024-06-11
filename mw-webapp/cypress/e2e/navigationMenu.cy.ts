3fec6af68d138ebb3580209eb389473c0d166505
EkaterinaSokolova-V <sokolovaekaterina.v@gmail.com>
2024-06-11 13:12:03 +0200

    feat: #1001 change array names

--- mw-webapp/cypress/e2e/navigationMenu.cy.ts
+++ mw-webapp/cypress/e2e/navigationMenu.cy.ts
@@ -63,18 +63,18 @@ describe('Navigation menu scope tests', () => {
     });
 
     it('NoAuth_NavMenu_Home_Language', () => {
-        const language = Object.keys(sideBarContent.language).map(key => key.toUpperCase());
-        const languageArray = Object.values(sideBarContent.language);
+        const languageCode = Object.keys(sideBarContent.language).map(key => key.toUpperCase());
+        const languageText = Object.values(sideBarContent.language);
 
         navigationMenuSelectors.language.getSelect().click();
 
-        language.forEach((languageCode, index) => {
-            navigationMenuSelectors.language.languageMenuItems[index]().should('have.text', languageCode);
+        languageCode.forEach((code, index) => {
+            navigationMenuSelectors.language.languageMenuItems[index]().should('have.text', code);
             navigationMenuSelectors.language.languageMenuItems[index]().click();
     
-            navigationMenuSelectors.language.getText().should('have.text', languageArray[index]);
+            navigationMenuSelectors.language.getText().should('have.text', languageText[index]);
 
-            if (index < languageArray.length - 1) {
+            if (index < languageText.length - 1) {
                 navigationMenuSelectors.language.getSelect().click();
               }
         });
