bae74613245fa13955e99d8df2624eeb159563dc
tritonJS826 <triton.68@mail.ru>
2024-06-12 06:05:19 +0400

    Revert "Mw 848/add oauth 2 (#1047)"
    This reverts commit a5e20b7869a9711bc1c290727be0781c73d693b1.

--- mw-webapp/src/logic/initializedApp/InitializedApp.tsx
+++ mw-webapp/src/logic/initializedApp/InitializedApp.tsx
@@ -1,10 +1,11 @@
 import {PropsWithChildren, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
-import {AuthDAL} from "src/dataAccessLogic/AuthDAL";
 import {useGlobalContext} from "src/GlobalContext";
 import {userStore} from "src/globalStore/UserStore";
 import {useErrorHandler} from "src/hooks/useErrorHandler";
+import {User} from "src/model/businessModel/User";
 import {pages} from "src/router/pages";
+import {AuthService} from "src/service/AuthService";
 
 /**
  * Check is current page is home page
@@ -32,27 +33,31 @@ export const InitializedApp = (props: PropsWithChildren) => {
   /**
    * OnLog in
    */
-  const recoverSessionIfPossible = async () => {
-    // TODO: loadUser if cookie "auth-session" exist
-    try {
-      const user = await AuthDAL.getAuthorizedUser();
-      setUser(user);
-      const defaultPagePath = getDefaultPagePath(user.uuid);
-      setIsInitialized(true);
+  const onLogIn = (user: User) => {
+    setUser(user);
+    const defaultPagePath = getDefaultPagePath(user.uuid);
 
-      if (getIsHomePage()) {
-        navigate(defaultPagePath);
-      }
-    } catch {
-      // eslint-disable-next-line no-console
-      console.warn("Session not recovered");
+    if (getIsHomePage()) {
+      navigate(defaultPagePath);
     }
+  };
 
+  /**
+   * OnLog out
+   */
+  const onLogOut = () => {
+    setUser(null);
+    const defaultPagePath = getDefaultPagePath(null);
+
+    if (getIsHomePage()) {
+      navigate(defaultPagePath);
+    }
   };
 
   useEffect(() => {
     if (!isInitialized) {
-      recoverSessionIfPossible();
+      AuthService.listenAuthStateChange({onLogIn, onLogOut});
+      setIsInitialized(true);
     }
   }, []);
 
