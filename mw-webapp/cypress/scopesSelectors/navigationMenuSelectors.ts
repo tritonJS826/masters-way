98ea85675dc36ec6700799017d2f900c1236bed0
EkaterinaSokolova-V <sokolovaekaterina.v@gmail.com>
2024-06-12 10:14:43 +0200

    feat: #1001 correct spelling errors1

--- mw-webapp/cypress/scopesSelectors/navigationMenuSelectors.ts
+++ mw-webapp/cypress/scopesSelectors/navigationMenuSelectors.ts
@@ -2,12 +2,14 @@ import {getDataCy} from "src/utils/cyTesting/getDataCy";
 import {navigationMenuIds} from "cypress/accessIds/navigationMenuIds";
 
 export const navigationMenuSelectors = {
+    getNavigationMenu: () => cy.get(getDataCy(navigationMenuIds.navigationMenu)),
+
     menuItemLinks: {
         getLogoItemLink: () => cy.get(getDataCy(navigationMenuIds.menuItemLinks.logo)),
         getHomeItemLink: () => cy.get(getDataCy(navigationMenuIds.menuItemLinks.home)),
         getAllWaysItemLink: () => cy.get(getDataCy(navigationMenuIds.menuItemLinks.allWays)),
         getAllUsersItemLink: () => cy.get(getDataCy(navigationMenuIds.menuItemLinks.allUsers)),
-        getAboutProjectItemLink: () => cy.get(getDataCy(navigationMenuIds.menuItemLinks.aboutProject))
+        getAboutProjectItemLink: () => cy.get(getDataCy(navigationMenuIds.menuItemLinks.aboutProject)),
     },
 
     language: {
@@ -23,5 +25,4 @@ export const navigationMenuSelectors = {
     },
 
     getCloseButton: () => cy.get(getDataCy(navigationMenuIds.closeButton))
-
 };
