770ef0d49273848e8a01ae81019a1c01b2a59821
Viktar Veratsennikau <56690686+tritonJS826@users.noreply.github.com>
2024-06-14 04:11:23 +0400

    Revert "Mw 848/add oauth 2 (#1059)" (#1060)
    This reverts commit 298ce0820eaa4e75cf6f9dd929bb545bb267b60e.

--- mw-server/auth/auth.go
+++ mw-server/auth/auth.go
@@ -1,80 +1,43 @@
 package auth
 
 import (
-	"errors"
-	"mwserver/config"
-	"time"
+	"log"
+	"mwserver/util"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
-	"golang.org/x/oauth2"
 )
-
-var GoogleOAuthConfig = &oauth2.Config{
-	RedirectURL:  config.Env.ApiBaseUrl + "/api/auth/google/callback",
-	ClientID:     config.Env.GooglClientId,
-	ClientSecret: config.Env.GooglClientSecret,
-	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-	Endpoint:     google.Endpoint,
-}
 
 const (
-	MaxAge           = 86400 * 30
-	OauthStateString = "auth-state-string"
-	AccessToken      = "access-token"
-	AuthStatePublic  = "auth-session-public"
+	MaxAge = 86400 * 5
 )
 
-var jwtKey = []byte(config.Env.SecretSessionKey)
-
-type Claims struct {
-	UserID string `json:"userID"`
-	jwt.StandardClaims
-}
-
-func GenerateJWT(userID string) (string, error) {
-	expirationTime := time.Now().Add(24 * time.Hour)
-	claims := &Claims{
-		UserID: userID,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(jwtKey)
-}
-
-func ValidateJWT(tokenString string) (*Claims, error) {
-	claims := &Claims{}
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return jwtKey, nil
-	})
+func NewAuth() {
+	config, err := util.LoadConfig(".")
 	if err != nil {
-		return nil, err
+		log.Fatalf("failed to load config: %v", err)
 	}
-	if !token.Valid {
-		return nil, errors.New("invalid token")
+
+	store := sessions.NewCookieStore([]byte(config.SecretSessionKey))
+	store.MaxAge(MaxAge)
+
+	store.Options.Path = "/"
+	store.Options.HttpOnly = config.EnvType != "prod"
+	store.Options.Secure = config.EnvType == "prod"
+
+	gothic.Store = store
+
+	var callBackUrl string
+	if config.EnvType == "prod" {
+		// TODO: move to env variables
+		callBackUrl = "https://mastersway.netlify.app"
+	} else {
+		callBackUrl = "http://localhost:5173"
 	}
-	return claims, nil
+
+	goth.UseProviders(
+		google.New(config.GooglClientId, config.GooglClientSecret, callBackUrl, "email", "profile"),
+	)
 }
-
-// func JWTMiddleware() gin.HandlerFunc {
-// 	return func(ctx *gin.Context) {
-// 		authHeader := ctx.GetHeader("Authorization")
-// 		if authHeader == "" {
-// 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
-// 			return
-// 		}
-
-// 		tokenString := authHeader[len("Bearer "):]
-// 		claims, err := ValidateJWT(tokenString)
-// 		if err != nil {
-// 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-// 			return
-// 		}
-
-// 		ctx.Set("userID", claims.UserID)
-// 		ctx.Next()
-// 	}
-// }
