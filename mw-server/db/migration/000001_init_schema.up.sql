d41ef56089bd7d059b016fed84bc940547f9d572
Kirill Smirnov <62110814+Moon1it@users.noreply.github.com>
2024-06-18 06:21:56 +0200

    fix: #988 Renamed check_max_mentors_in_way_limit to check_max_mentors… (#1091)
    * fix: #988 Renamed check_max_mentors_in_way_limit to check_max_mentors_in_way; Fixed condition for check_max_mentors_in_way and check_max_mentoring_ways_for_user.
    
    * fix: #988 Corrected comment.
    
    * fix: #988 Corrected error messages.
    
    ---------
    
    Co-authored-by: Moon1it <Moon1it@icloud.com>

--- mw-server/db/migration/000001_init_schema.up.sql
+++ mw-server/db/migration/000001_init_schema.up.sql
@@ -94,7 +94,7 @@ CREATE TABLE "metrics"(
 
 CREATE TABLE "job_tags"(
     "uuid" UUID NOT NULL DEFAULT (uuid_generate_v4()),
-    "name" VARCHAR(20) NOT NULL,
+    "name" VARCHAR(30) NOT NULL,
     "description" VARCHAR(100) NOT NULL,
     "color" VARCHAR(8) NOT NULL,
     "way_uuid" UUID NOT NULL REFERENCES ways("uuid") ON UPDATE CASCADE ON DELETE CASCADE,
@@ -321,10 +321,10 @@ FOR EACH ROW
 EXECUTE FUNCTION check_max_ways_for_former_mentor();
 
 -- максимальное число менторов в одном пути
-CREATE OR REPLACE FUNCTION check_max_mentors_in_way_limit()
+CREATE OR REPLACE FUNCTION check_max_mentors_in_way()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (SELECT COUNT(*) FROM mentor_users_ways WHERE user_uuid = NEW.user_uuid) > 30 THEN
+    IF (SELECT COUNT(*) FROM mentor_users_ways WHERE way_uuid = NEW.way_uuid) > 30 THEN
         RAISE EXCEPTION 'Exceeded a limit of 30 mentors in a single way';
     END IF;
     RETURN NEW;
@@ -334,7 +334,7 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER max_mentors_in_way_trigger
 BEFORE INSERT ON mentor_users_ways
 FOR EACH ROW
-EXECUTE FUNCTION check_max_mentors_in_way_limit();
+EXECUTE FUNCTION check_max_mentors_in_way();
 
 -- максимальное число путей в одной коллекции
 CREATE OR REPLACE FUNCTION check_ways_in_collection()
@@ -502,7 +502,7 @@ EXECUTE FUNCTION check_max_foremer_mentors_in_way();
 CREATE OR REPLACE FUNCTION check_max_mentoring_ways_for_user()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (SELECT COUNT(*) FROM mentor_users_ways WHERE way_uuid = NEW.way_uuid) > 50000 THEN
+    IF (SELECT COUNT(*) FROM mentor_users_ways WHERE user_uuid = NEW.user_uuid) > 50000 THEN
         RAISE EXCEPTION 'Exceeded a limit of 50000 ways where a used can be named a mentor';
     END IF;
     RETURN NEW;
