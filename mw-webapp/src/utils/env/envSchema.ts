a95d0731e0f046d906547549062abcabb8439f51
Ekaterina1994 <katishteno@mail.ru>
2024-06-19 15:18:12 +0300

    feat: #1103 Add ga4

--- mw-webapp/src/utils/env/envSchema.ts
+++ mw-webapp/src/utils/env/envSchema.ts
@@ -10,4 +10,6 @@ export const envSchema = {
   FIREBASE_MEASUREMENT_ID: str(), // Need if you want to use google analytics
   IS_LOGGER_ENABLED: str(),
   API_BASE_PATH: str(),
+  GOOGLE_MEASUREMENT_ID: str(),
+  ENV_TYPE: str(),
 };
