f19951686821001c730840cbb07e1820fe9e8cea
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-06-19 15:39:53 +0500

    feat: #1007 paralleled requests and data conversion

--- mw-server/services/user.service.go
+++ mw-server/services/user.service.go
@@ -203,7 +203,6 @@ func dbFavoriteWaysToDbWays(rawWay []dbb.GetFavoriteWaysByUserIdRow) []dbWay {
 		}
 	})
 }
-
 func GetPopulatedUserById(db *dbb.Queries, ctx context.Context, userUuid uuid.UUID) (schemas.UserPopulatedResponse, error) {
 	user, err := db.GetUserById(ctx, userUuid)
 
@@ -341,6 +340,205 @@ func GetPopulatedUserById(db *dbb.Queries, ctx context.Context, userUuid uuid.UU
 			IsMentor:    dbUser.IsMentor,
 		}
 	})
+
+	response := schemas.UserPopulatedResponse{
+		Uuid:               user.Uuid.String(),
+		Name:               user.Name,
+		Email:              user.Email,
+		Description:        user.Description,
+		CreatedAt:          user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+		ImageUrl:           user.ImageUrl,
+		IsMentor:           user.IsMentor,
+		WayCollections:     wayCollections,
+		DefaultCollections: defaultCollections,
+		FavoriteForUsers:   favoriteForUsersUuid,
+		FavoriteUsers:      favoriteUsers,
+		Tags:               tags,
+		WayRequests:        wayRequests,
+	}
+
+	return response, err
+}
+
+func GetPopulatedUserByIdParallel(db *dbb.Queries, ctx context.Context, userUuid uuid.UUID) (schemas.UserPopulatedResponse, error) {
+	user, err := db.GetUserById(ctx, userUuid)
+
+	ownWaysChan := make(chan []schemas.WayPlainResponse)
+	mentoringWaysChan := make(chan []schemas.WayPlainResponse)
+	favoriteWaysChan := make(chan []schemas.WayPlainResponse)
+	wayCollectionsChan := make(chan []schemas.WayCollectionPopulatedResponse)
+	tagsChan := make(chan []schemas.UserTagResponse)
+	wayRequestsChan := make(chan []schemas.WayPlainResponse)
+	favoriteForUsersUuidChan := make(chan []string)
+	favoriteUsersChan := make(chan []schemas.UserPlainResponse)
+
+	go func() {
+		dbOwnWays, _ := db.GetOwnWaysByUserId(ctx, user.Uuid)
+		ownWays := convertDbWaysToPlainWays(db, ctx, dbOwnWaysToDbWays(dbOwnWays))
+		ownWaysChan <- ownWays
+	}()
+	go func() {
+		dbMentoringWays, _ := db.GetMentoringWaysByMentorId(ctx, user.Uuid)
+		mentoringWays := convertDbWaysToPlainWays(db, ctx, dbMentoringWaysToDbWays(dbMentoringWays))
+		mentoringWaysChan <- mentoringWays
+	}()
+	go func() {
+		dbFavoriteWays, _ := db.GetFavoriteWaysByUserId(ctx, user.Uuid)
+		favoriteWays := convertDbWaysToPlainWays(db, ctx, dbFavoriteWaysToDbWays(dbFavoriteWays))
+		favoriteWaysChan <- favoriteWays
+	}()
+
+	go func() {
+		dbWayCollections, _ := db.GetWayCollectionsByUserId(ctx, user.Uuid)
+		wayCollections := lo.Map(dbWayCollections, func(collection dbb.WayCollection, i int) schemas.WayCollectionPopulatedResponse {
+			dbCollectionWays, _ := db.GetWaysByCollectionId(ctx, collection.Uuid)
+			dbCollectionWaysPrepared := dbCollectionWaysToDbWays(dbCollectionWays)
+
+			ways := convertDbWaysToPlainWays(db, ctx, dbCollectionWaysPrepared)
+
+			wayCollection := schemas.WayCollectionPopulatedResponse{
+				Uuid:      collection.Uuid.String(),
+				Name:      collection.Name,
+				Ways:      ways,
+				CreatedAt: collection.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+				UpdatedAt: collection.UpdatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+				OwnerUuid: collection.OwnerUuid.String(),
+				Type:      string(collection.Type),
+			}
+			return wayCollection
+		})
+		wayCollectionsChan <- wayCollections
+	}()
+
+	go func() {
+		tagsRaw, _ := db.GetListUserTagsByUserId(ctx, user.Uuid)
+		tags := lo.Map(tagsRaw, func(dbUserTag dbb.UserTag, i int) schemas.UserTagResponse {
+			return schemas.UserTagResponse{
+				Name: dbUserTag.Name,
+				Uuid: dbUserTag.Uuid.String(),
+			}
+		})
+		tagsChan <- tags
+	}()
+
+	go func() {
+		wayRequestsRaw, _ := db.GetFromUserMentoringRequestWaysByUserId(ctx, user.Uuid)
+		wayRequests := lo.Map(wayRequestsRaw, func(dbWay dbb.GetFromUserMentoringRequestWaysByUserIdRow, i int) schemas.WayPlainResponse {
+			copiedFromWayUuid := util.MarshalNullUuid(dbWay.CopiedFromWayUuid)
+			dbWayTags, _ := db.GetListWayTagsByWayId(ctx, dbWay.Uuid)
+			wayTags := lo.Map(dbWayTags, func(dbWayTag dbb.WayTag, i int) schemas.WayTagResponse {
+				return schemas.WayTagResponse{
+					Uuid: dbWayTag.Uuid.String(),
+					Name: dbWayTag.Name,
+				}
+			})
+			dbMentors, _ := db.GetMentorUsersByWayId(ctx, dbWay.Uuid)
+			mentors := lo.Map(dbMentors, func(dbMentor dbb.User, i int) schemas.UserPlainResponse {
+				return schemas.UserPlainResponse{
+					Uuid:        dbMentor.Uuid.String(),
+					Name:        dbMentor.Name,
+					Email:       dbMentor.Email,
+					Description: dbMentor.Description,
+					CreatedAt:   dbMentor.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+					ImageUrl:    dbMentor.ImageUrl,
+					IsMentor:    dbMentor.IsMentor,
+				}
+			})
+			dbOwner, _ := db.GetUserById(ctx, dbWay.Uuid)
+			owner := schemas.UserPlainResponse{
+				Uuid:        dbOwner.Uuid.String(),
+				Name:        dbOwner.Name,
+				Email:       dbOwner.Email,
+				Description: dbOwner.Description,
+				CreatedAt:   dbOwner.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+				ImageUrl:    dbOwner.ImageUrl,
+				IsMentor:    dbOwner.IsMentor,
+			}
+			return schemas.WayPlainResponse{
+				Uuid:              dbWay.Uuid.String(),
+				Name:              dbWay.Name,
+				GoalDescription:   dbWay.GoalDescription,
+				UpdatedAt:         dbWay.UpdatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+				CreatedAt:         dbWay.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+				EstimationTime:    dbWay.EstimationTime,
+				IsCompleted:       dbWay.IsCompleted,
+				Owner:             owner,
+				CopiedFromWayUuid: copiedFromWayUuid,
+				IsPrivate:         dbWay.IsPrivate,
+				FavoriteForUsers:  int32(dbWay.WayFavoriteForUsers),
+				DayReportsAmount:  int32(dbWay.WayDayReportsAmount),
+				Mentors:           mentors,
+				WayTags:           wayTags,
+				MetricsDone:       int32(dbWay.WayMetricsDone),
+				MetricsTotal:      int32(dbWay.WayMetricsTotal),
+				ChildrenUuids:     dbWay.ChildrenUuids,
+			}
+		})
+		wayRequestsChan <- wayRequests
+	}()
+
+	go func() {
+		favoriteForUsersUuidRaw, _ := db.GetFavoriteUserUuidsByAcceptorUserId(ctx, user.Uuid)
+		favoriteForUsersUuid := lo.Map(favoriteForUsersUuidRaw, func(uuid uuid.UUID, i int) string {
+			return uuid.String()
+		})
+		favoriteForUsersUuidChan <- favoriteForUsersUuid
+	}()
+
+	go func() {
+		favoriteUsersRaw, _ := db.GetFavoriteUserByDonorUserId(ctx, user.Uuid)
+		favoriteUsers := lo.Map(favoriteUsersRaw, func(dbUser dbb.GetFavoriteUserByDonorUserIdRow, i int) schemas.UserPlainResponse {
+			return schemas.UserPlainResponse{
+				Uuid:        dbUser.Uuid.String(),
+				Name:        dbUser.Name,
+				Email:       dbUser.Email,
+				Description: dbUser.Description,
+				CreatedAt:   dbUser.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+				ImageUrl:    dbUser.ImageUrl,
+				IsMentor:    dbUser.IsMentor,
+			}
+		})
+		favoriteUsersChan <- favoriteUsers
+	}()
+
+	ownWays := <-ownWaysChan
+	mentoringWays := <-mentoringWaysChan
+	favoriteWays := <-favoriteWaysChan
+	wayCollections := <-wayCollectionsChan
+	tags := <-tagsChan
+	wayRequests := <-wayRequestsChan
+	favoriteForUsersUuid := <-favoriteForUsersUuidChan
+	favoriteUsers := <-favoriteUsersChan
+
+	defaultCollections := schemas.DefaultWayCollections{
+		Own: schemas.WayCollectionPopulatedResponse{
+			Uuid:      "00000000-0000-0000-0000-00000000001",
+			Name:      "Own",
+			Ways:      ownWays,
+			CreatedAt: user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+			UpdatedAt: user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+			OwnerUuid: user.Uuid.String(),
+			Type:      "own",
+		},
+		Mentoring: schemas.WayCollectionPopulatedResponse{
+			Uuid:      "00000000-0000-0000-0000-00000000002",
+			Name:      "Mentoring",
+			Ways:      mentoringWays,
+			CreatedAt: user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+			UpdatedAt: user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+			OwnerUuid: user.Uuid.String(),
+			Type:      "mentoring",
+		},
+		Favorite: schemas.WayCollectionPopulatedResponse{
+			Uuid:      "00000000-0000-0000-0000-00000000003",
+			Name:      "Favorite",
+			Ways:      favoriteWays,
+			CreatedAt: user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+			UpdatedAt: user.CreatedAt.Format(util.DEFAULT_STRING_LAYOUT),
+			OwnerUuid: user.Uuid.String(),
+			Type:      "favorite",
+		},
+	}
 
 	response := schemas.UserPopulatedResponse{
 		Uuid:               user.Uuid.String(),
