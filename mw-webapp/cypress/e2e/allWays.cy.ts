8c2d655e9948cbc24edab4077b451d04e4349c92
Ekaterina <115548501+EkaterinaSokolova-V@users.noreply.github.com>
2024-06-27 12:41:16 +0200

    feat: #1118 NoAuth_AllWays_SelectTableView test (#1119)
    * feat: #1118 NoAuth_AllWays_SelectTableView test
    
    * feat: #1118 edit AllWays_SelectTableView test
    
    * feat: #1118 edit NoAuth_AllWays_SelectTableView test
    
    * feat: #1118 edit NoAuth_AllWays_SelectTableView

--- mw-webapp/cypress/e2e/allWays.cy.ts
+++ mw-webapp/cypress/e2e/allWays.cy.ts
@@ -1,13 +1,33 @@
 import {allWaysSelectors} from "cypress/scopesSelectors/allWaysSelectors";
-import allWaysPageData from "cypress/fixtures/allWaysFixture.json";
 import allWaysPageContent from "src/dictionary/AllWaysPageContent.json";
 import {headerSelectors} from "cypress/scopesSelectors/headerSelectors";
 import {navigationMenuSelectors} from "cypress/scopesSelectors/navigationMenuSelectors";
+import {Symbols} from "src/utils/Symbols";
 
 describe('All Ways scope tests', () => {
 
   beforeEach(() => {
     cy.visit('/');
+    headerSelectors.getBurgerMenu().click();
+    navigationMenuSelectors.menuItemLinks.getAllWaysItemLink().click();
   });
 
+  afterEach(() => {
+    cy.clearAllStorage();
+  });
+
+  it('NoAuth_AllWays_SelectTableView', () => {
+    const tableHeadersEn = Object.values(allWaysPageContent.waysTable.columns).map(column => column.en);
+    tableHeadersEn.push(Symbols.STAR);
+
+    allWaysSelectors.filterViewBlock.getTableViewButton().click();
+
+    allWaysSelectors.allWaysTable.getTable().should('exist');
+
+    allWaysSelectors.allWaysTable.getTableTh().each((el, index) => {
+      cy.wrap(el).invoke('text').then((text) => {
+      expect(text.trim()).equal(tableHeadersEn[index]);
+      });
+    });
+  });
 });
