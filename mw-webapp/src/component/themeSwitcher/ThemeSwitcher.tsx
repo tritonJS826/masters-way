53c9bf5f56d69981ef71aa39d69a261cff39136a
kladweb <kladpav@gmail.com>
2024-07-01 11:40:26 +0300

    feat: #989 add accessibility in buttons on Home page [Pavel Kladkevich] (#1207)
    * feat: #989 add accessibility in buttons on Home page [Pavel Kladkevich]
    
    * fix: #989 correct aria-label for theme switch button (#1207) [Pavel Kladkevich]

--- mw-webapp/src/component/themeSwitcher/ThemeSwitcher.tsx
+++ mw-webapp/src/component/themeSwitcher/ThemeSwitcher.tsx
@@ -78,6 +78,7 @@ export const ThemeSwitcher = (props: ThemeSwitcherProps) => {
         className={clsx(styles.iconWrapper, props.className)}
         onClick={onChangeTheme}
         data-cy={props.dataCy}
+        aria-label={getDescriptionForTheme(props.theme, props.language)}
       >
         <Icon
           size={IconSize.MEDIUM}
