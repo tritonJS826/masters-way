4e8a911d699642bc7d346d59d4260c948a44ebfd
Ekaterina <115548501+EkaterinaSokolova-V@users.noreply.github.com>
2024-07-19 23:00:06 +0200

    feat: #1297 add IsAuth_UserPersonalData_EditUserName test (#1318)

--- mw-webapp/src/component/title/Title.cy.tsx
+++ mw-webapp/src/component/title/Title.cy.tsx
@@ -4,7 +4,7 @@ import {HeadingLevel, Title} from "src/component/title/Title";
 import {getDataCy} from "src/utils/cyTesting/getDataCy";
 
 const TITLE_CONTENT = "Title";
-const TITLE_CY = "title";
+const TITLE_CY = {dataCyTitleContainer: "title"};
 const TITLE_LEVEL = HeadingLevel.h1;
 const IS_EDITABLE = true;
 const ADDITIONAL_CONTENT = "additional";
@@ -21,7 +21,7 @@ describe("Title component", () => {
       <Title
         text={TITLE_CONTENT}
         level={TITLE_LEVEL}
-        dataCy={TITLE_CY}
+        cy={TITLE_CY}
         isEditable={IS_EDITABLE}
         onChangeFinish={STUB_FUNCTION}
         placeholder=""
@@ -32,7 +32,7 @@ describe("Title component", () => {
 
   it("render right value", () => {
     mountTitle();
-    cy.get(getDataCy(TITLE_CY)).should("contains.text", TITLE_CONTENT);
+    cy.get(getDataCy(TITLE_CY.dataCyTitleContainer)).should("contains.text", TITLE_CONTENT);
     cy.get(TITLE_LEVEL).should("exist");
   });
 
@@ -43,12 +43,12 @@ describe("Title component", () => {
 
   it("should change to input on doubleClick and onChangeFinished triggered", () => {
     mountTitle();
-    cy.get(getDataCy(TITLE_CY)).dblclick();
+    cy.get(getDataCy(TITLE_CY.dataCyTitleContainer)).dblclick();
     cy.get("input[type=\"text\"]")
       .type(ADDITIONAL_CONTENT)
       .type("{enter}");
 
-    cy.get(getDataCy(TITLE_CY)).should("contains.text", TITLE_CONTENT + ADDITIONAL_CONTENT);
+    cy.get(getDataCy(TITLE_CY.dataCyTitleContainer)).should("contains.text", TITLE_CONTENT + ADDITIONAL_CONTENT);
     cy.wrap(STUB_FUNCTION).should("have.been.called");
   });
 
