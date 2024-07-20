e78512fae3b8decc779d9dea33873de5d011898f
Moon1it <viza0010@gmail.com>
2024-07-20 19:23:56 +0200

    feat: #1307 Added test service.

--- mw-chat/internal/services/p2p_rooms.service.go
+++ mw-chat/internal/services/p2p_rooms.service.go
@@ -6,3 +6,14 @@ type P2PRoomsService struct {
 func NewP2PRoomsController() *P2PRoomsService {
 	return &P2PRoomsService{}
 }
+
+func (p *P2PRoomsService) TestService() {
+
+	// Chat
+
+	// General
+
+	// Processing
+
+	return
+}
