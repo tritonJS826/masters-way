16b1b84b06ef8df9ee2e97e852436ce526941cb1
EkaterinaSokolova-V <sokolovaekaterina.v@gmail.com>
2024-07-22 21:02:47 +0200

    feat: #1297 add IsAuth_UserPersonalData_EditAboutSection test

--- mw-webapp/cypress/scopesSelectors/userPersonalDataSelectors.ts
+++ mw-webapp/cypress/scopesSelectors/userPersonalDataSelectors.ts
@@ -5,6 +5,8 @@ export const userPersonalSelectors = {
     descriptionSection: {
         getName: () => cy.get(getDataCy(userPersonalDataAccessIds.descriptionSection.nameDisplay)),
         getNameInput: () => cy.get(getDataCy(userPersonalDataAccessIds.descriptionSection.nameInput)),
+        getAboutMe: () => cy.get(getDataCy(userPersonalDataAccessIds.descriptionSection.aboutMeMarkdownDisplay)),
+        getAboutMeInput: () => cy.get(getDataCy(userPersonalDataAccessIds.descriptionSection.aboutMeMarkdownInput)),
         getAddSkillButton: () => cy.get(getDataCy(userPersonalDataAccessIds.descriptionSection.addSkillButton)),
     }
 };
