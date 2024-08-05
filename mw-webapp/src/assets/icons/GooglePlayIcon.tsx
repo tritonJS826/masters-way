bba2956d8574cefa8bb8f0eb22341c6e5639b94d
AKiroi <alexandr.kiroi@gmail.com>
2024-08-05 21:32:39 +0300

    feat: #1314 add appStore buttons

---
+++ mw-webapp/src/assets/icons/GooglePlayIcon.tsx
@@ -0,0 +1,38 @@
+import {IconProps} from "src/component/icon/Icon";
+
+/**
+ * GooglePlay icon
+ */
+export const GooglePlayIcon = (props: IconProps) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={props.className}
+    >
+      <path
+        d="M3 19.2876V4.7119C3 3.39426 4.60967 2.57223 5.89407 3.23345L20.0404 10.5213C20.3298 10.6624
+        20.5722 10.8748 20.7414 11.1355C20.9106 11.3961 21 11.695 21 11.9997C21 12.3045 20.9106 12.6034
+        20.7414 12.864C20.5722 13.1247 20.3298 13.3371 20.0404 13.4782L5.89535 20.766C4.60967 21.4285 3 20.6052 3 19.2876Z"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.59668 3.47943L13.0464 11.6157L17.0192 15.0358"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.55664 20.472L13.045 11.6157L16.2849 8.59161"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+
+  );
+};
+
