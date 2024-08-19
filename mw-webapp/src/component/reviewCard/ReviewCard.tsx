1566a9d0bd70afc10bd3c5744f0e793d42b665fd
Ekaterina1994 <katishteno@mail.ru>
2024-08-19 22:19:35 +0300

    fix: #1462 Added reviewCard component

---
+++ mw-webapp/src/component/reviewCard/ReviewCard.tsx
@@ -0,0 +1,66 @@
+import {Avatar, AvatarSize} from "src/component/avatar/Avatar";
+import {HorizontalContainer} from "src/component/horizontalContainer/HorizontalContainer";
+import {Icon, IconSize} from "src/component/icon/Icon";
+import {VerticalContainer} from "src/component/verticalContainer/VerticalContainer";
+import styles from "src/component/reviewCard/ReviewCard.module.scss";
+
+/**
+ * ReviewCard props
+ */
+interface ReviewCardProps {
+
+  /**
+   * Amount of stars of our app
+   */
+  gradeAmount: number;
+
+  /**
+   * Review from user
+   */
+  review: string;
+
+  /**
+   * Reviewer name
+   */
+  reviewerName: string;
+
+  /**
+   * Reviewer image url
+   */
+  reviewerImageUrl: string;
+
+}
+
+/**
+ * WayCollectionCard component
+ */
+export const ReviewCard = (props: ReviewCardProps) => {
+
+  const gradeAmountArray = [...Array(props.gradeAmount).keys()].map(i => i++);
+
+  return (
+    <VerticalContainer className={styles.reviewCard}>
+      <HorizontalContainer className={styles.gradeBlock}>
+        {gradeAmountArray.map((gradeItem) => (
+          <Icon
+            name="StarIcon"
+            size={IconSize.MEDIUM}
+            key={gradeItem}
+            className={styles.gradeItem}
+          />
+        ))}
+      </HorizontalContainer>
+      <p className={styles.review}>
+        {props.review}
+      </p>
+      <VerticalContainer className={styles.reviewerBlock}>
+        <Avatar
+          alt={`Reviewer ${props.reviewerName}`}
+          src={props.reviewerImageUrl}
+          size={AvatarSize.BIG}
+        />
+        {props.reviewerName}
+      </VerticalContainer>
+    </VerticalContainer>
+  );
+};
