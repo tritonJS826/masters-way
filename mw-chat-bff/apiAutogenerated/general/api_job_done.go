d7b0772cc34bcdd00f7cb414433b90ed20e7f43e
Viktar Veratsennikau <56690686+tritonJS826@users.noreply.github.com>
2024-08-20 02:47:21 +0300

    feat: #1178 fix cors (#1467)

--- mw-chat-bff/apiAutogenerated/general/api_job_done.go
+++ mw-chat-bff/apiAutogenerated/general/api_job_done.go
@@ -118,7 +118,7 @@ func (a *JobDoneAPIService) CreateJobDoneExecute(r ApiCreateJobDoneRequest) (*Sc
 			error: localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 403 {
-			var v CustomErrorsNoRightToChangeDayReportError
+			var v UtilNoRightToChangeDayReportError
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 			if err != nil {
 				newErr.error = err.Error()
