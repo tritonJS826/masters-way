61e5e25382b57afe528b3af2df1eeb5c17721f57
Viktar Veratsennikau <56690686+tritonJS826@users.noreply.github.com>
2024-09-08 07:14:56 +0400

    feat: #1552 db update storage schemas (#1537)

--- mw-storage/internal/db/migration/000001_init_schema.down.sql
+++ mw-storage/internal/db/migration/000001_init_schema.down.sql
@@ -1,7 +1,13 @@
+DROP TABLE IF EXISTS files;
+DROP TYPE IF EXISTS file_extension;
+
+
+-- previous - deprecated
 DROP TABLE IF EXISTS message_status;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS users_rooms;
 DROP TABLE IF EXISTS rooms;
+
 
 DROP TYPE IF EXISTS user_role_type;
 DROP TYPE IF EXISTS room_type;
--- mw-storage/internal/db/migration/000001_init_schema.up.sql
+++ mw-storage/internal/db/migration/000001_init_schema.up.sql
@@ -1,39 +1,14 @@
 SET TIMEZONE = 'UTC';
 
-CREATE TYPE room_type AS ENUM ('private', 'group');
-CREATE TABLE rooms (
+CREATE TYPE file_extension AS ENUM ('webp', 'mp4', 'mp3');
+
+CREATE TABLE files (
     "uuid" UUID NOT NULL DEFAULT uuid_generate_v4(),
-    "name" VARCHAR(50),
-    "type" room_type NOT NULL,
+    "link" VARCHAR(500),
+    "ownerId" UUID NOT NULL,
+    "size" BIGINT NOT NULL CHECK (size BETWEEN 0 AND 10_737_418_240), -- 10Gb
+    "type" file_extension NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "rooms_pkey" PRIMARY KEY (uuid)
+    CONSTRAINT "files_pkey" PRIMARY KEY (uuid)
 );
 
-CREATE TYPE user_role_type AS ENUM ('admin', 'regular');
-CREATE TABLE users_rooms (
-    "user_uuid" UUID NOT NULL,
-    "room_uuid" UUID NOT NULL REFERENCES rooms("uuid") ON UPDATE CASCADE ON DELETE CASCADE,
-    "user_role" user_role_type NOT NULL,
-    "is_room_blocked" BOOLEAN NOT NULL DEFAULT FALSE,
-    "joined_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "users_rooms_pkey" PRIMARY KEY ("user_uuid", "room_uuid")
-);
-
-CREATE TABLE messages (
-    "uuid" UUID NOT NULL DEFAULT uuid_generate_v4(),
-    "owner_uuid" UUID NOT NULL,
-    "room_uuid" UUID NOT NULL REFERENCES rooms("uuid") ON UPDATE CASCADE ON DELETE CASCADE,
-    "text" VARCHAR(4096) NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "messages_pkey" PRIMARY KEY ("uuid")
-);
-
-CREATE TABLE message_status (
-    "message_uuid" UUID NOT NULL REFERENCES messages("uuid") ON UPDATE CASCADE ON DELETE CASCADE,
-    "receiver_uuid" UUID NOT NULL,
-    "is_read" bool NOT NULL DEFAULT FALSE,
-    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "message_status_pkey" PRIMARY KEY ("message_uuid", "receiver_uuid")
-);
