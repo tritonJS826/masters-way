9ea0223d62fe21205bb5fbfa16ba47358fd6ca70
AKiroi <112805668+AKiroi@users.noreply.github.com>
2024-09-14 14:47:50 +0300

    feat: #1511 create slider (#1538)
    * feat: #1511 create slider
    
    * fix: #1511 remove comments
    
    * fix: #1511 update slider props
    
    * fix: #1511 update SliderSettings
    
    * fix: #1511 update SliderSettings
    
    * fix: #1511 remove comments
    
    * fix: #1511 fixed slider storybook
    
    * fix: #1511 remove revieCards
    
    * fix: #1511 fixed slider breakpoint
    
    * fix: #1511 updated slider breakpoint

---
+++ mw-webapp/src/component/slider/Sliders.stories.tsx
@@ -0,0 +1,51 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {Slider, SliderItem} from "src/component/slider/Slider";
+
+const getDivElement = (content: string) =>
+  (<div style={{height: 500, display: "flex", justifyContent: "center", alignItems: "center", background: "#888"}}>
+    {content}
+  </div>);
+
+const sliderItems: SliderItem[] = [
+  {id: 1, content: getDivElement("Slide 1")},
+  {id: 2, content: getDivElement("Slide 2")},
+  {id: 3, content: getDivElement("Slide 3")},
+  {id: 4, content: getDivElement("Slide 4")},
+];
+
+const meta = {
+  title: "Slider",
+  component: Slider,
+  parameters: {layout: "centered"},
+  tags: ["autodocs"],
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj;
+
+export const DefaultSlider: Story = {
+  args: {
+    sliderItems,
+    amountSlider: 3,
+    gap: 24,
+  },
+};
+
+export const SliderWithScrollbar: Story = {
+  args: {
+    sliderItems,
+    amountSlider: 2,
+    gap: 20,
+    settings: {scrollbar: {draggable: true}},
+  },
+};
+
+export const NoneLoopingSlider: Story = {
+  args: {
+    sliderItems,
+    amountSlider: 1,
+    gap: 10,
+    settings: {loop: false},
+  },
+};
