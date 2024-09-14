3c4f0be037c85387f0bc6e3a7f7b95dc7495e76f
AKiroi <112805668+AKiroi@users.noreply.github.com>
2024-09-14 14:47:50 +0300

    feat: #1511 create slider (#1538)
    * feat: #1511 create slider
    
    * fix: #1511 remove comments
    
    * fix: #1511 update slider props
    
    * fix: #1511 update SliderSettings
    
    * fix: #1511 update SliderSettings
    
    * fix: #1511 remove comments
    
    * fix: #1511 fixed slider storybook
    
    * fix: #1511 remove revieCards
    
    * fix: #1511 fixed slider breakpoint
    
    * fix: #1511 updated slider breakpoint

---
+++ mw-webapp/src/component/slider/Slider.tsx
@@ -0,0 +1,150 @@
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import {Navigation, Pagination, Scrollbar} from "swiper/modules";
+import {Swiper, SwiperProps, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import "src/component/slider/Slider.scss";
+
+const DEFAULT_AMOUNT_SLIDER = 3;
+const DEFAULT_GAP_SLIDER = 24;
+
+/**
+ * Breakpoint configuration
+ */
+interface BreakpointConfig {
+
+  /**
+   * Slides per view
+   */
+  slidesPerView: number;
+}
+
+/**
+ * Breakpoints object type
+ */
+type Breakpoints = {
+  [width: number]: BreakpointConfig;
+};
+
+/**
+ * Slider settings
+ */
+interface SliderSettings {
+
+  /**
+   * Navigation settings
+   */
+  navigation?: SwiperProps["navigation"];
+
+  /**
+   * Pagination settings
+   */
+  pagination?: SwiperProps["pagination"];
+
+  /**
+   * Scrollbar settings
+   */
+  scrollbar?: SwiperProps["scrollbar"];
+
+  /**
+   * Loop settings
+   */
+  loop?: SwiperProps["loop"];
+}
+
+/**
+ * Slider item
+ */
+export interface SliderItem {
+
+  /**
+   * Item id
+   */
+  id: number;
+
+  /**
+   * Item content
+   */
+  content: React.ReactNode;
+}
+
+/**
+ * Slider props
+ */
+interface SliderProps {
+
+  /**
+   * Slider items
+   */
+  sliderItems: SliderItem[];
+
+  /**
+   * Data attributes for cypress testing
+   */
+  cy?: string;
+
+  /**
+   * Custom class for the Slider.
+   */
+  className?: string;
+
+  /**
+   * Amount slider items
+   * @default Number 3
+   */
+  amountSlider?: number;
+
+  /**
+   * Gap between slider items
+   * @default Number 24
+   */
+  gap?: number;
+
+  /**
+   * Slider settings
+   */
+  settings?: SliderSettings;
+
+  /**
+   * Custom breakpoints
+   */
+  breakpoints?: Breakpoints;
+}
+
+/**
+ * Slider component
+ */
+export const Slider = (props: SliderProps) => {
+
+  const defaultBreakpoints: Breakpoints = {
+    320: {slidesPerView: 1},
+    600: {slidesPerView: 2},
+    1100: {slidesPerView: 3},
+  };
+
+  const defaultSettings: SliderSettings = {
+    navigation: true,
+    pagination: {clickable: true},
+    loop: true,
+  };
+
+  const processedSettings = {...defaultSettings, ...props?.settings};
+
+  return (
+    <Swiper
+      slidesPerView={props.amountSlider ?? DEFAULT_AMOUNT_SLIDER}
+      spaceBetween={props.gap ?? DEFAULT_GAP_SLIDER}
+      breakpoints={props.breakpoints ?? defaultBreakpoints}
+      modules={[Navigation, Pagination, Scrollbar]}
+      className={props.className}
+      {...processedSettings}
+    >
+      {props.sliderItems.map((slider) => (
+        <SwiperSlide key={slider.id}>
+          {slider.content}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
