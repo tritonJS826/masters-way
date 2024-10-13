0534a0c2bd0ae0a9dde6f135ba52beb268e5fa75
mikromolekula2002 <149586429+mikromolekula2002@users.noreply.github.com>
2024-10-13 17:43:29 +0300

    Revert "mail: #1609 Create mail service for issue"

--- mw-mail/README.md
+++
@@ -1,7 +0,0 @@
-# mw-mail
-
-- reset db
-
-```
-curl http://localhost:8003/mail/dev/reset-db
-```
--- mw-mail/internal/db/query/file.query.sql
+++
@@ -1,4 +0,0 @@
--- name: CreateLogMail :one
-INSERT INTO mail_logs (from_email, from_name, recipients, cc, bcc, subject, message, html_message, err)
-VALUES (@from_email, @from_name, @recipients, @cc, @bcc, @subject, @message, @html_message, @err)
-RETURNING uuid, from_email, recipients, subject, message, html_message, err;
