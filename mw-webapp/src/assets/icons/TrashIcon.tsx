4959ae760a1e75445a901875dfdc5f92d7facce2
Ekaterina <57002521+Ekaterina1994@users.noreply.github.com>
2024-10-20 23:40:42 +0300

    fix: #1515 Fixed long nospaces words in the chat render, added trash Icon from feather instead of react icons for consistency, fixed label line styles on mobile (adjust labels modal) (#1646)

---
+++ mw-webapp/src/assets/icons/TrashIcon.tsx
@@ -0,0 +1,22 @@
+import {IconProps} from "src/component/icon/Icon";
+
+/**
+ * Trash icon
+ */
+export const TrashIcon = (props: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      className={props.className}
+      data-cy={props.dataCy}
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="3 6 5 6 21 6" />
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+    </svg>
+  );
+};
