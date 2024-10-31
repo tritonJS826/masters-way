c9b09d2e2a33907cbbd1c86c4fe9c3871502c88b
Artem <timoxa201354@gmail.com>
2024-10-31 10:51:38 +0200

    The styles on the page have been updated, an image and two Home and Back buttons have been added.

--- mw-webapp/src/component/privateRecourse/PrivateRecourse.tsx
+++ mw-webapp/src/component/privateRecourse/PrivateRecourse.tsx
@@ -1,6 +1,12 @@
-import {HeadingLevel, Title} from "src/component/title/Title";
-import {VerticalContainer} from "src/component/verticalContainer/VerticalContainer";
+import { HeadingLevel, Title } from "src/component/title/Title";
+import { VerticalContainer } from "src/component/verticalContainer/VerticalContainer";
 import styles from "src/component/privateRecourse/PrivateRecourse.module.scss";
+import { Button, ButtonType } from "../button/Button";
+import { HorizontalContainer } from "../horizontalContainer/HorizontalContainer";
+import { LanguageService } from "src/service/LanguageService";
+import { useNavigate } from "react-router-dom";
+import { languageStore } from "src/globalStore/LanguageStore";
+import { Image } from "../image/Image";
 
 /**
  * Private recourse props
@@ -22,16 +28,41 @@ interface ErrorComponentProps {
  * Private recourse render info about private pages
  */
 export const ErrorComponent = (props: ErrorComponentProps) => {
+
+  const navigate = useNavigate();
+  const { language } = languageStore;
+
   return (
     <VerticalContainer className={styles.container}>
-      <Title
-        text={props.text}
-        level={HeadingLevel.h1}
-        placeholder={""}
-      />
-      <div className={styles.description}>
-        {props.description}
-      </div>
+      <VerticalContainer className={styles.Inner}>
+        <Title
+          className={styles.Title}
+          text={props.text}
+          level={HeadingLevel.h1}
+          placeholder={""}
+        />
+        <HorizontalContainer className={styles.ButtonGroup}>
+          <Button
+            onClick={() => navigate({ pathname: "/" })}
+            buttonType={ButtonType.PRIMARY}
+            value={LanguageService.user.button.home[language]}
+            className={styles.Button}
+          />
+          <Button
+            onClick={() => navigate(-1)}
+            buttonType={ButtonType.SECONDARY}
+            value={LanguageService.user.button.back[language]}
+          />
+        </HorizontalContainer>
+        <div className={styles.description}>
+          {props.description}
+        </div>
+        <img
+          alt="group"
+          src={"src/assets/Group.png"}
+          className={styles.ImageBackground}
+        />
+      </VerticalContainer>
     </VerticalContainer>
   );
 };
