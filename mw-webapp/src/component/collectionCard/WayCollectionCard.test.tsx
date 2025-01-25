e50a4be81996f79c1373c4524a650e187ebb0c38
AKiroi <112805668+AKiroi@users.noreply.github.com>
2025-01-25 06:33:37 +0200

    feat: #1377 rewrite tests wayCardCollection (#1848)
    * feat: #1377 rewrite tests wayCardCollection
    
    * fix: #1377 fixed ViewSwitcher test
    
    * fix: #1377 add setupTest configuration
    
    * fix: #1377 remove default params to switcher
    
    * fix: #1377 rename comment
    
    * fix: #1377 remove import to switcher
    
    * fix: #1377 remove import to switcher
    
    * fix: #1377 fixed  switcher
    
    * fix: #1377 fixed wayCArd
    
    * fix: #1377 refactor tests in one styles
    
    * fix: #1377 refactor tests in one styles
    
    * fix: #1377 refactor tests in one styles
    
    * fix: #1377 fix WayCard test

---
+++ mw-webapp/src/component/collectionCard/WayCollectionCard.test.tsx
@@ -0,0 +1,37 @@
+import {render, screen} from "@testing-library/react";
+import {WayCollectionCard} from "src/component/wayCollectionCard/WayCollectionCard";
+import {Language} from "src/globalStore/LanguageStore";
+
+const WAY_COLLECTION_CARD = "wayCollectionCard";
+const WAY_COLLECTION_CARD_TITLE = "WayCollectionCard Title";
+const WAYS_AMOUNT = 9;
+
+/**
+ * Render WayCollectionCard
+ */
+const renderWayCollectionCard = () =>
+  render(
+    <WayCollectionCard
+      isActive={false}
+      collectionTitle={WAY_COLLECTION_CARD_TITLE}
+      collectionWaysAmount={WAYS_AMOUNT}
+      onClick={() => {}}
+      dataCy={WAY_COLLECTION_CARD}
+      language={Language.ENGLISH}
+    />,
+  );
+
+describe("WayCollectionCard component", () => {
+  it("should render wayCollectionCard correctly", () => {
+    renderWayCollectionCard();
+    const card = screen.getByTestId(WAY_COLLECTION_CARD);
+    expect(card).toBeInTheDocument();
+  });
+
+  it("should display the correct content elements", () => {
+    renderWayCollectionCard();
+    const card = screen.getByTestId(WAY_COLLECTION_CARD);
+    expect(card).toHaveTextContent(WAY_COLLECTION_CARD_TITLE);
+    expect(card).toHaveTextContent(WAYS_AMOUNT.toString());
+  });
+});
