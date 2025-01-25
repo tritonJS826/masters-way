0b915561826f30ed2424ab4041532b51ec2f4ab6
akiroi <alexandr.kiroi@gmail.com>
2025-01-25 14:23:22 +0200

    feat: #1377 rewrite Toggle tests

---
+++ mw-webapp/src/component/title/Title.test.tsx
@@ -0,0 +1,64 @@
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {HeadingLevel, Title} from "src/component/title/Title";
+import {describe, expect, it, vi} from "vitest";
+
+const TITLE_CONTENT = "Title";
+const TITLE = "title";
+const TITLE_CY = {dataCyTitleContainer: "title"};
+const TITLE_LEVEL = HeadingLevel.h1;
+const IS_EDITABLE = true;
+const ADDITIONAL_CONTENT = "additional";
+
+describe("Title component", () => {
+
+  /**
+   * Renders Title component
+   */
+  const renderTitle = (onChangeFinish = vi.fn()) => {
+    render(
+      <Title
+        text={TITLE_CONTENT}
+        level={TITLE_LEVEL}
+        cy={TITLE_CY}
+        isEditable={IS_EDITABLE}
+        onChangeFinish={onChangeFinish}
+        placeholder=""
+      />,
+    );
+  };
+
+  it("should render the correct value", () => {
+    renderTitle();
+    const titleContainer = screen.getByTestId(TITLE);
+
+    expect(titleContainer).toHaveTextContent(TITLE_CONTENT);
+  });
+
+  it("should render the appropriate heading level (check semantics)", () => {
+    renderTitle();
+    expect(screen.getByRole("heading", {level: 1})).toBeInTheDocument();
+  });
+
+  it("should change to input on double-click and trigger onChangeFinish", async () => {
+    const mockOnChangeFinish = vi.fn();
+    const user = userEvent.setup();
+
+    renderTitle(mockOnChangeFinish);
+
+    const titleContainer = screen.getByTestId(TITLE);
+
+    await user.dblClick(titleContainer);
+
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+
+    await user.type(input, ADDITIONAL_CONTENT);
+    await user.keyboard("{Enter}");
+
+    expect(screen.getByTestId(TITLE)).toHaveTextContent(
+      TITLE_CONTENT + ADDITIONAL_CONTENT,
+    );
+    expect(mockOnChangeFinish).toHaveBeenCalled();
+  });
+});
---
+++ mw-webapp/src/component/tooltip/Tooltip.test.tsx
@@ -0,0 +1,55 @@
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {Button} from "src/component/button/Button";
+import {PositionTooltip} from "src/component/tooltip/PositionTooltip";
+import {Tooltip} from "src/component/tooltip/Tooltip";
+import {vi} from "vitest";
+
+const TOOLTIP_CONTENT = "Tooltip value";
+const TOOLTIP = "tooltip";
+const TOOLTIP_TRIGGER = "tooltip trigger";
+
+describe("Tooltip component", () => {
+
+  /**
+   * Render tooltip
+   */
+  const renderTooltip = () => render(
+    <Tooltip
+      content={TOOLTIP_CONTENT}
+      dataCy={TOOLTIP}
+      position={PositionTooltip.BOTTOM}
+    >
+      <Button
+        value={TOOLTIP_TRIGGER}
+        onClick={() => {}}
+        dataCy={TOOLTIP_TRIGGER}
+      />
+    </Tooltip>,
+  );
+
+  it("should be hidden by default", () => {
+    renderTooltip();
+
+    const trigger = screen.getByTestId(TOOLTIP_TRIGGER);
+    expect(trigger).toBeVisible();
+
+    const tooltip = screen.queryByTestId(TOOLTIP);
+    expect(tooltip).toBeNull();
+  });
+
+  it("should show tooltip on hover and render right value", async () => {
+    const user = userEvent.setup();
+    renderTooltip();
+
+    const trigger = screen.getByTestId(TOOLTIP_TRIGGER);
+
+    await user.hover(trigger);
+
+    await vi.waitFor(() => {
+      const tooltip = screen.getByTestId(TOOLTIP);
+      expect(tooltip).toBeVisible();
+      expect(tooltip).toHaveTextContent(TOOLTIP_CONTENT);
+    });
+  });
+});
