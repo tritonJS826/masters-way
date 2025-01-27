6365edd06556acebe654e23fe57db05d7daa5ff7
Ion <vanea.ss21@gmail.com>
2025-01-27 22:01:33 +0100

    fix: #1830 added variables in styles

--- mw-webapp/src/assets/icons/MessageIcon.tsx
+++ mw-webapp/src/assets/icons/MessageIcon.tsx
@@ -10,7 +10,6 @@ export const MessageIcon = (props: IconProps) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
