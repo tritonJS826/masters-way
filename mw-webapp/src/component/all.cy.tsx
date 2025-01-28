342698a554431d1ec5269903471b70fec3076b63
AKiroi <112805668+AKiroi@users.noreply.github.com>
2025-01-28 08:22:09 +0200

    feat: #1377 rewrite textarea tests (#1857)
    * feat: #1377 rewrite textarea tests
    
    * fix: #1377 fix ThemeSwitcher test
    
    * fix: #1377 fix ThemedImage component
    
    * fix: #1377 remove comments to ThemedImage component

--- mw-webapp/src/component/all.cy.tsx
+++ mw-webapp/src/component/all.cy.tsx
@@ -13,7 +13,6 @@ import "/src/component/scrollableBlock/ScrollableBlock.cy.tsx";
 import "/src/component/select/Select.cy.tsx";
 import "/src/component/sidebar/Sidebar.cy.tsx";
 import "/src/component/table/Table.cy.tsx";
-import "/src/component/textarea/Textarea.cy.tsx";
 import "/src/component/icon/Icon.cy.tsx";
 import "/src/component/text/Text.cy.tsx";
 import "src/component/reviewCard/ReviewCard.cy.tsx";
