0ddaf2656f8f085f0519e252020361fb99e915ff
Pavel Kladkevich <kladpav@gmail.com>
2025-01-29 08:43:52 +0300

    refactor: #1377 Add tests for Footer component

--- mw-webapp/src/component/footer/Footer.test.tsx
+++ mw-webapp/src/component/footer/Footer.test.tsx
@@ -1,18 +1,21 @@
 import {BrowserRouter} from "react-router-dom";
 import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import {Footer} from "src/component/footer/Footer";
 import {LOGO_TEXT} from "src/component/header/Header";
 import {languageStore} from "src/globalStore/LanguageStore";
+import {LanguageService} from "src/service/LanguageService";
 
 const FOOTER_CY = "footer";
+const language = languageStore.language;
 
-describe("Footer", () => {
+describe("Footer component", () => {
   beforeEach(() => {
     render(
       <BrowserRouter>
         <Footer
           dataCy={FOOTER_CY}
-          language={languageStore.language}
+          language={language}
         />
       </BrowserRouter>,
     );
@@ -24,6 +27,38 @@ describe("Footer", () => {
 
   it("Logotype image should be visible if source exists", () => {
     expect(screen.getByRole("img")).toBeVisible();
+  });
+
+  it("All app stores buttons should be visible and open modal window on click", async () => {
+    const buttons = screen.getAllByRole("button");
+    for (let i = 0; i < buttons.length; i++) {
+      expect(buttons[i]).toBeVisible();
+      expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+      await userEvent.click(buttons[i]);
+      const modalDialog = screen.getByRole("dialog");
+      expect(modalDialog).toBeVisible();
+      await userEvent.click(screen.getByRole("button"));
+      expect(modalDialog).not.toBeInTheDocument();
+    }
+  });
+
+  it("All social media links should be visible", () => {
+    const linkedInLabel = LanguageService.common.socialMediaAriaLabel.linkedIn[language];
+    const linkedInLink = screen.getByRole("link", {name: linkedInLabel});
+    expect(linkedInLink).toBeVisible();
+
+    const youtubeLabel = LanguageService.common.socialMediaAriaLabel.youtube[language];
+    const youtubeLink = screen.getByRole("link", {name: youtubeLabel});
+    expect(youtubeLink).toBeVisible();
+
+    const patreonLabel = LanguageService.common.socialMediaAriaLabel.patreon[language];
+    const patreonLink = screen.getByRole("link", {name: patreonLabel});
+    expect(patreonLink).toBeVisible();
+  });
+
+  it("Privacy policy link should be visible", () => {
+    const privacyPolicyName = LanguageService.common.privacyPolicy[language];
+    expect(screen.getByText(privacyPolicyName)).toBeVisible();
   });
 
 });
