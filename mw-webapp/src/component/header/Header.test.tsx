c3dfe2ecaa3d5f48ba8ee0f5ec116b63cc83cfbe
soniikot <soniikot@gmail.com>
2025-01-30 19:12:01 -0800

    refactor: #1377 Rewrote tests from cypress to vitest for Header component

--- mw-webapp/src/component/header/Header.test.tsx
+++ mw-webapp/src/component/header/Header.test.tsx
@@ -5,52 +5,16 @@ import {Header, LOGO_TEXT} from "src/component/header/Header";
 import {testUserPreview} from "src/component/header/testUserPreview";
 import {languageStore} from "src/globalStore/LanguageStore";
 import {themeStore} from "src/globalStore/ThemeStore";
+import {MockPointerEvent} from "src/utils/mockPointerEventForVitest";
 import {vi} from "vitest";
 
 const HEADER_CY = "header";
 
-/**
- * The MockPointerEvent class overrides the default PointerEvent to ensure the dropdown interactions work in tests.
- */
-export class MockPointerEvent extends Event {
-
-  /**
-   * Indicates which button was pressed on the mouse or pointer device.
-   * 0 = Left button, 1 = Middle button, 2 = Right button.
-   */
-  private button: number | undefined;
-
-  /**
-   * Indicates whether the Ctrl key was pressed during the event.
-   */
-  private ctrlKey: boolean | undefined;
-
-  constructor(type: string, props: PointerEventInit | undefined) {
-    super(type, props);
-    if (props) {
-      if (props.button !== null) {
-        this.button = props.button;
-      }
-      if (props.ctrlKey !== null) {
-        this.ctrlKey = props.ctrlKey;
-      }
-    }
-  }
-
-}
-
-window.PointerEvent = MockPointerEvent as unknown as typeof PointerEvent;
-window.HTMLElement.prototype.scrollIntoView = vi.fn();
-window.HTMLElement.prototype.hasPointerCapture = vi.fn();
-window.HTMLElement.prototype.releasePointerCapture = vi.fn();
-
 describe("Header component", () => {
-  let STUB_FUNCTION_SET_LANGUAGE: () => void;
-  let STUB_FUNCTION_SET_THEME: () => void;
+  const STUB_FUNCTION_SET_LANGUAGE: () => void = vi.fn();
+  const STUB_FUNCTION_SET_THEME: () => void = vi.fn();
 
   beforeEach(() => {
-    STUB_FUNCTION_SET_LANGUAGE = vi.fn();
-    STUB_FUNCTION_SET_THEME = vi.fn();
     render(
       <BrowserRouter>
         <Header
@@ -93,12 +57,10 @@ describe("Header component", () => {
         button: 0,
       }),
     );
-
     expect(triggerButton).toHaveAttribute("aria-expanded", "true");
-    const uaOption = await screen.findByText("UA");
-    expect(uaOption).toBeVisible();
-    await userEvent.click(uaOption);
 
+    const uaOption = await screen.findByText("UA");
+    await userEvent.click(uaOption);
     expect(STUB_FUNCTION_SET_LANGUAGE).toHaveBeenCalled();
   });
 });
