336212b1dd1ab673408f8751060a98a3cc4a3287
tritonJS826 <triton.68@mail.ru>
2025-01-31 11:25:30 +0100

    feat: #1766 added training mentor

--- mw-training-bff/internal/controllers/trainingMentor.controller .go
+++ mw-training-bff/internal/controllers/trainingMentor.controller .go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"mw-training-bff/internal/services"
+	util "mw-training-bff/internal/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -26,25 +27,17 @@ func NewTrainingMentorController(generalService *services.GeneralService, traini
 // @Param userId path string true "user id"
 // @Success 200
 // @Router /trainingMentors/{trainingId}/user/{userId} [post]
-func (nc *TrainingMentorController) CreateMentor(ctx *gin.Context) {
-	// userUUID := ctx.Value(auth.ContextKeyUserID).(string)
-	// page := ctx.DefaultQuery("page", "1")
-	// limit := ctx.DefaultQuery("limit", "50")
-	// isOnlyNew := ctx.DefaultQuery("isOnlyNew", "false")
+func (tmc *TrainingMentorController) CreateMentor(ctx *gin.Context) {
+	trainingId := ctx.Param("trainingId")
+	mentorId := ctx.Param("userId")
 
-	// reqPage, _ := strconv.Atoi(page)
-	// reqLimit, _ := strconv.Atoi(limit)
-	// reqIsOnlyNew, err := strconv.ParseBool(isOnlyNew)
+	args := &services.CreateTrainingMentorParams{
+		MentorUuid:   mentorId,
+		TrainingUuid: trainingId,
+	}
 
-	// getNotificationListParams := &services.GetNotificationListParams{
-	// 	UserUUID:  userUUID,
-	// 	Page:      int32(reqPage),
-	// 	Limit:     int32(reqLimit),
-	// 	IsOnlyNew: reqIsOnlyNew,
-	// }
-
-	// response, err := nc.notificationService.GetNotificationList(ctx, getNotificationListParams)
-	// utils.HandleErrorGin(ctx, err)
+	err := tmc.trainingMentorService.CreateTrainingMentor(ctx, args)
+	util.HandleErrorGin(ctx, err)
 
 	ctx.Status(http.StatusOK)
 }
@@ -59,17 +52,17 @@ func (nc *TrainingMentorController) CreateMentor(ctx *gin.Context) {
 // @Param userId path string true "user id"
 // @Success 200
 // @Router /trainingMentors/{trainingId}/user/{userId} [delete]
-func (nc *TrainingMentorController) DeleteMentor(ctx *gin.Context) {
-	// var payload *schemas.UpdateNotificationPayload
-	// notificationUUID := ctx.Param("notificationId")
+func (tmc *TrainingMentorController) DeleteMentor(ctx *gin.Context) {
+	trainingId := ctx.Param("trainingId")
+	mentorId := ctx.Param("userId")
 
-	// if err := ctx.ShouldBindJSON(&payload); err != nil {
-	// 	ctx.JSON(http.StatusBadRequest, gin.H{"status": "Failed payload", "error": err.Error()})
-	// 	return
-	// }
+	args := &services.DeleteTrainingMentorParams{
+		MentorUuid:   mentorId,
+		TrainingUuid: trainingId,
+	}
 
-	// response, err := nc.notificationService.UpdateNotification(ctx, notificationUUID, payload.IsRead)
-	// utils.HandleErrorGin(ctx, err)
+	err := tmc.trainingMentorService.DeleteTrainingMentorByIds(ctx, args)
+	util.HandleErrorGin(ctx, err)
 
 	ctx.Status(http.StatusOK)
 }
