b6aff9d873bae8b7389239624a7e5e8e26b09030
Pavel Kladkevich <kladpav@gmail.com>
2025-02-16 23:00:57 +0300

    fix: #1377 Fixed Icon tests, its made by a single test

--- mw-webapp/src/component/icon/Icon.test.tsx
+++ mw-webapp/src/component/icon/Icon.test.tsx
@@ -35,19 +35,12 @@ const renderTestIcon = (props: createTestIconProps) => {
 
 describe("Icon component", () => {
 
-  it("should be render big size", () => {
-    renderTestIcon({name: ICON, size: IconSize.BIG});
-    expect(screen.getByTestId(ICON_CY)).toBeInTheDocument();
-  });
-
-  it("should be render medium size", () => {
-    renderTestIcon({name: ICON, size: IconSize.MEDIUM});
-    expect(screen.getByTestId(ICON_CY)).toBeInTheDocument();
-  });
-
-  it("should be render small size", () => {
-    renderTestIcon({name: ICON, size: IconSize.SMALL});
-    expect(screen.getByTestId(ICON_CY)).toBeInTheDocument();
+  it("All icon sizes should be rendered", () => {
+    Object.keys(IconSize).forEach((icon) => {
+      renderTestIcon({name: ICON, size: IconSize[icon as keyof typeof IconSize]});
+    });
+    const icons = screen.getAllByTestId(ICON_CY);
+    expect(icons.length).toEqual(Object.keys(IconSize).length);
   });
 
 });
