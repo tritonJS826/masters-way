947105fd07cbcbebe17697ca6615fee3d629ce7e
tritonJS826 <triton.68@mail.ru>
2025-02-28 17:22:59 +0100

    fix: #1766 multiple tags on all trainings page

--- mw-training/internal/db/query/trainings.query.sql
+++ mw-training/internal/db/query/trainings.query.sql
@@ -40,10 +40,10 @@ SELECT
     trainings.updated_at,
     trainings.is_private,
     COALESCE(f.favorite_count, 0) AS favorite_count,
-    ARRAY_AGG(training_tags.name) FILTER (WHERE training_tags.name IS NOT NULL)::VARCHAR[] AS tags,
-    ARRAY_AGG(trainings_mentors.mentor_uuid) FILTER (WHERE trainings_mentors.mentor_uuid IS NOT NULL)::UUID[] AS training_mentors,
-    ARRAY_AGG(trainings_students.student_uuid) FILTER (WHERE trainings_students.student_uuid IS NOT NULL)::UUID[] AS training_students,
-    COUNT(topics.uuid) FILTER (WHERE topics.uuid IS NOT NULL) AS topics_count
+    ARRAY_AGG(DISTINCT training_tags.name) FILTER (WHERE training_tags.name IS NOT NULL)::VARCHAR[] AS tags,
+    ARRAY_AGG(DISTINCT trainings_mentors.mentor_uuid) FILTER (WHERE trainings_mentors.mentor_uuid IS NOT NULL)::UUID[] AS training_mentors,
+    ARRAY_AGG(DISTINCT trainings_students.student_uuid) FILTER (WHERE trainings_students.student_uuid IS NOT NULL)::UUID[] AS training_students,
+    COUNT(DISTINCT topics.uuid) FILTER (WHERE topics.uuid IS NOT NULL) AS topics_count
 FROM
     trainings
 LEFT JOIN
