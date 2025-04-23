3a1cc190ed211340e62e109ebc4c7e8b20765654
soniikot <soniikot@gmail.com>
2025-04-23 11:00:11 -0700

    feat: #1929 delete file created by mistake

--- src/model/businessModel/User.ts
+++
@@ -1,5 +0,0 @@
-this.favoriteUsers = userData.favoriteUsers;
-this.skills = userData.skills.map(skill => new Skill(skill));
-this.contacts = userData.contacts?.map(contact => new Contact(contact)) ?? [];
-this.imageUrl = userData.imageUrl;
-this.isMentor = userData.isMentor; 
