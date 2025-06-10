e3bb8f72c2ab949bc9dabae4a2e623b603de9d9f
Ekaterina Sokolova <sokolovaekaterina.v@gmail.com>
2025-06-10 14:32:46 +0200

    feat: #1966 visual tests with @simonsmith/cypress-image-snapshot/plugin

--- mw-webapp/cypress.config.ts
+++ mw-webapp/cypress.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
+import {addMatchImageSnapshotPlugin} from '@simonsmith/cypress-image-snapshot/plugin';
+// import fs from "fs-extra";
+// import path from "path";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -37,6 +40,46 @@ const cypressConfig = defineConfig({
         ...config.env,
         ...process.env,
       };
+
+      addMatchImageSnapshotPlugin(on);
+
+    on('before:browser:launch', (browser, launchOptions) => {
+      const targetWidth = 1920;
+      const targetHeight = 1080;
+
+      // For Electron
+      if (browser.name === 'electron') {
+        launchOptions.preferences.width = targetWidth;
+        launchOptions.preferences.height = targetHeight;
+        launchOptions.preferences.resizable = false;
+      }
+      // For Chrome, Edge, etc. (Chromium)
+      else if (browser.family === 'chromium') {
+        launchOptions.args.push(`--window-size=${targetWidth},${targetHeight}`);
+        launchOptions.args.push('--start-maximized');
+      }
+      // For Firefox
+      else if (browser.family === 'firefox') {
+        launchOptions.args.push(`--width=${targetWidth}`);
+        launchOptions.args.push(`--height=${targetHeight}`);
+      }
+
+      // Дополнительно отключаем автоматическое масштабирование
+      // launchOptions.args.push('--force-device-scale-factor=1');
+      // launchOptions.args.push('--disable-dev-shm-usage');
+
+      return launchOptions;
+    });
+      // on('task', {
+      //   deleteFolder(folderPath: string) {
+      //     const fullPath = path.resolve(folderPath);
+      //     if (fs.existsSync(fullPath)) {
+      //       fs.removeSync(fullPath);
+      //       return true;
+      //     }
+      //     return false;
+      //   },
+      // });
 
       // Load environment variables from .env file
       return config;
