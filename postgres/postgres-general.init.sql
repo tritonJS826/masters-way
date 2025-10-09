91fbdf196044707c34b08f7a5211ef1e9ed384d7
tritonJS826 <triton.68@mail.ru>
2025-10-09 03:55:55 +0200

    feat: #1 added exmaple pg stats

--- postgres/postgres-general.init.sql
+++ postgres/postgres-general.init.sql
@@ -1,3 +1,14 @@
 \c mastersway_db
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+-- usage example:
+-- SELECT
+--     queryid,
+--     calls,
+--     round(total_exec_time::numeric, 2) AS total_time_ms,
+--     round(mean_exec_time::numeric, 2) AS avg_time_ms,
+--     rows,
+--     LEFT(query, 30) AS query_snippet  -- первые 200 символов
+-- FROM pg_stat_statements
+-- ORDER BY mean_exec_time DESC
